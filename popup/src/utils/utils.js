885d1c5617b94c02e87ea9027ed2df9f36bf94fc
Szymon Lesisz <sz.lesisz@gmail.com>
2017-10-11 17:13:18 +0200

    fetch with same-orgin

--- popup/src/utils/utils.js
+++ popup/src/utils/utils.js
@@ -3,7 +3,7 @@ import semvercmp from 'semver-compare';
 import * as _ from 'lodash';
 
 export const httpRequest = (url: string, json: boolean): any => {
-    return fetch(url).then((response) => {
+    return fetch(url, { credentials: 'same-origin' }).then((response) => {
         if (response.status === 200) {
             return response.text().then(result => (json ? JSON.parse(result) : result));
         } else {
