9c04bb578b2ac6965d8a314b3dcb2735cef70518
Szymon Lesisz <sz.lesisz@gmail.com>
2018-03-19 10:57:32 +0100

    server on https

--- webpack/server.js
+++ webpack/server.js
@@ -4,6 +4,8 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import open from 'open';
+import fs from 'fs';
+import https from 'https';
 import { spawn } from 'child_process';
 import { argv } from 'yargs';
 import chokidar from 'chokidar';
@@ -38,13 +40,29 @@ app.get('*', function(req, res) {
     res.sendFile(HTML_SRC + req.params[0]);
 });
 
-app.listen(PORT, 'localhost', function(err) {
+// app.listen(PORT, 'localhost', function(err) {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     open(`http://localhost:${PORT}/`);
+//     console.log(`Listening at http://localhost:${PORT}`);
+// });
+
+var key = fs.readFileSync('./webpack/key.pem');
+var cert = fs.readFileSync('./webpack/cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
+
+https.createServer(https_options, app).listen(PORT, 'localhost', function(err) {
     if (err) {
         console.log(err);
         return;
     }
-    open(`http://localhost:${PORT}/`);
-    console.log(`Listening at http://localhost:${PORT}`);
+    open(`https://localhost:${PORT}/`);
+    console.log(`Listening at https://localhost:${PORT}`);
 });
 
 
