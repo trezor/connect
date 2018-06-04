e75d5c6066c32270f808adf0b496c5e2bfe27bcf
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-04 14:26:24 +0200

    MInor fixes & implement '.passphrase-on-device' view

--- src/js/popup/view/requestButton.js
+++ src/js/popup/view/requestButton.js
@@ -4,18 +4,20 @@
 import { container, showView } from './common';
 
 export const requestButton = (data: Object): void => {
-    showView('simple-message');
+    // showView('simple-message');
 
-    const h3: HTMLElement = container.getElementsByTagName('h3')[0];
-    const div: HTMLElement = container.getElementsByClassName('message')[0];
+    // const h3: HTMLElement = container.getElementsByTagName('h3')[0];
+    // const div: HTMLElement = container.getElementsByClassName('message')[0];
 
-    h3.innerHTML = ''; // 'Wait for button action...';
-    div.innerHTML = '';
+    // h3.innerHTML = ''; // 'Wait for button action...';
+    // div.innerHTML = '';
 
     if (data.code === 'ButtonRequest_ConfirmOutput') {
-        h3.innerHTML = 'Check recipient address on your device and follow further instructions.';
+        // h3.innerHTML = 'Check recipient address on your device and follow further instructions.';
+        showView('check-address');
     } else {
-        h3.innerHTML = 'Follow instructions on your device.';
+        // h3.innerHTML = 'Follow instructions on your device.';
+        showView('follow-device');
     }
     // div.innerHTML = `Button code: ${data}`;
     // div.innerHTML = `Check recipient address on your device`;
