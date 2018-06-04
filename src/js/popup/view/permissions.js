906f3c34f9007a0705bec7e3fcbaf12ed7be01e7
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-04 13:21:49 +0200

    Fix 'permissions.js' & add a tooltip box

--- src/js/popup/view/permissions.js
+++ src/js/popup/view/permissions.js
@@ -5,25 +5,59 @@ import { UiMessage } from '../../core/CoreMessage';
 import * as UI from '../../constants/ui';
 import { container, showView, postMessage } from './common';
 
+const getPermissionText = (permissionType: string, deviceName: string): string => {
+    let text: string = '';
+
+    switch (permissionType) {
+        case 'read':
+            text = `Read data from ${deviceName}`;
+            break;
+        case 'write':
+            text = `Write data to ${deviceName}`;
+            break;
+    }
+    return text;
+};
+
+const createTooltip = (text: string): HTMLDivElement => {
+    const infoIcon: HTMLDivElement = document.createElement('div');
+    infoIcon.className = 'info-icon';
+
+    const tooltip: HTMLDivElement = document.createElement('div');
+    tooltip.className = 'tooltip';
+    const tooltipText = document.createElement('span')
+    tooltipText.appendChild(
+        document.createTextNode(text)
+    );
+    tooltip.appendChild(tooltipText);
+
+    infoIcon.appendChild(tooltip);
+    return infoIcon;
+};
+
 export const initPermissionsView = (data: any, origin: string): void => {
     showView('permissions');
 
     const h3: HTMLElement = container.getElementsByTagName('h3')[0];
     const hostName: HTMLElement = h3.getElementsByTagName('span')[0];
-    const list: HTMLElement = container.getElementsByClassName('permissions-list')[0];
-    const confirmButton: HTMLElement = container.getElementsByClassName('confirm-button')[0];
-    const cancelButton: HTMLElement = container.getElementsByClassName('cancel-button')[0];
+    const permissionsList: HTMLElement = container.getElementsByClassName('permissions-list')[0];
+    const confirmButton: HTMLElement = container.getElementsByClassName('confirm')[0];
+    const cancelButton: HTMLElement = container.getElementsByClassName('cancel')[0];
 
     hostName.innerHTML = origin;
     if (data && Array.isArray(data)) {
-        const ul: HTMLUListElement = document.createElement('ul');
-        ul.className = 'permissions_list';
-        for (const p of data) {
-            const li: HTMLLIElement = document.createElement('li');
-            li.innerHTML = p;
-            ul.append(li);
-        }
-        list.append(ul);
+        data.forEach(p => {
+            const listItem: HTMLLIElement = document.createElement('li');
+
+            const tooltip = createTooltip('TODO: Change text here');
+            listItem.appendChild(tooltip);
+
+            const permissionText = getPermissionText(p, '#TREZOR');
+            listItem.appendChild(
+                document.createTextNode(permissionText)
+            );
+            permissionsList.appendChild(listItem);
+        });
     }
 
     confirmButton.onclick = () => {
