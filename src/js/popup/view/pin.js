8747ccd791fda49e13fdc77ddb09e5cddf0dd52e
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-04 11:24:18 +0200

    Fixes for '.pin' view
    Disable submit button when pin is empty, set max length of pin. Minor style edits to fit the input value and distinguish a disabled button from a regular button.

--- src/js/popup/view/pin.js
+++ src/js/popup/view/pin.js
@@ -5,20 +5,42 @@ import { UiMessage } from '../../core/CoreMessage';
 import * as UI from '../../constants/ui';
 import { container, showView, postMessage } from './common';
 
+const isSubmitButtonDisabled = (isDisabled: boolean) => {
+    const submitButton: HTMLElement = container.getElementsByClassName('submit')[0];
+    if (isDisabled) {
+        submitButton.setAttribute('disabled', 'true');
+    } else {
+        submitButton.removeAttribute('disabled');
+    }
+};
+
 const submit = (): void => {
     const button = container.getElementsByClassName('submit')[0];
     button.click();
 };
 
-const addPinFromKeyboard = (nr: number): void => {
+const addPin = (val: number): void => {
     const input: HTMLInputElement = (container.getElementsByClassName('pin-input')[0]: any);
-    if (input.value.length < 9) { input.value += nr; }
-};
+    const maxInputLength = 9;
+
+    if (input.value.length < maxInputLength) {
+        input.value += val;
+
+        if (input.value.length > 0) {
+            isSubmitButtonDisabled(false);
+        }
+    }
+}
 
 const backspacePin = (): void => {
     const input: HTMLInputElement = (container.getElementsByClassName('pin-input')[0]: any);
     const pin = input.value;
+
     input.value = pin.substring(0, pin.length - 1);
+
+    if (!input.value) {
+        isSubmitButtonDisabled(true);
+    }
 };
 
 const pinKeyboardHandler = (event: KeyboardEvent): void => {
@@ -36,39 +58,39 @@ const pinKeyboardHandler = (event: KeyboardEvent): void => {
         // numeric and numpad
         case 49 :
         case 97 :
-            addPinFromKeyboard(1);
+            addPin(1);
             break;
         case 50 :
         case 98 :
-            addPinFromKeyboard(2);
+            addPin(2);
             break;
         case 51 :
         case 99 :
-            addPinFromKeyboard(3);
+            addPin(3);
             break;
         case 52 :
         case 100 :
-            addPinFromKeyboard(4);
+            addPin(4);
             break;
         case 53 :
         case 101 :
-            addPinFromKeyboard(5);
+            addPin(5);
             break;
         case 54 :
         case 102 :
-            addPinFromKeyboard(6);
+            addPin(6);
             break;
         case 55 :
         case 103 :
-            addPinFromKeyboard(7);
+            addPin(7);
             break;
         case 56 :
         case 104 :
-            addPinFromKeyboard(8);
+            addPin(8);
             break;
         case 57 :
         case 105 :
-            addPinFromKeyboard(9);
+            addPin(9);
             break;
     }
 };
@@ -84,27 +106,26 @@ export const initPinView = (payload: any): void => {
 
     header.innerHTML = header.innerHTML.replace('#TREZOR', payload.device.label);
 
-    let i: number;
-    const len: number = buttons.length;
-
-    const handleClick = (event: MouseEvent) => {
-        if (event.target instanceof HTMLElement) {
-            const val: ?string = event.target.getAttribute('data-value');
-            if (val) { input.value += val; }
-        }
-    };
-
-    for (i = 0; i < len; i++) {
-        buttons.item(i).addEventListener('click', handleClick);
+    for (let i = 0; i < buttons.length; i++) {
+        buttons.item(i).addEventListener('click', (event: MouseEvent) => {
+            if (event.target instanceof HTMLElement) {
+                const val: ?string = event.target.getAttribute('data-value');
+                if (val) {
+                    addPin(+val);
+                }
+            }
+        });
     }
 
     backspace.addEventListener('click', backspacePin);
 
     enter.addEventListener('click', (event: MouseEvent) => {
-        window.removeEventListener('keydown', pinKeyboardHandler, false);
+        if (input.value.length > 0) {
+            window.removeEventListener('keydown', pinKeyboardHandler, false);
 
-        showView('loader');
-        postMessage(new UiMessage(UI.RECEIVE_PIN, input.value));
+            showView('loader');
+            postMessage(new UiMessage(UI.RECEIVE_PIN, input.value));
+        }
     });
 
     window.addEventListener('keydown', pinKeyboardHandler, false);
