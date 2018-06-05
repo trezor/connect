43ff651297539a7c0781e3f4e2ee940c49cbd43d
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-05 14:01:30 +0200

    Minor changes to layout for export-account and export-xpub views

--- src/js/popup/view/confirmation.js
+++ src/js/popup/view/confirmation.js
@@ -6,13 +6,20 @@ import * as UI from '../../constants/ui';
 import { container, showView, postMessage } from './common';
 
 export const initConfirmationView = (data: any): void => {
+    // Confirmation views:
+    // - export xpub
+    // - export account info
+
+    // TODO: Check if correct class names for HTML views
     showView(data.view);
 
-    const h3: HTMLElement = container.getElementsByTagName('h3')[0];
-    const confirmButton: HTMLElement = container.getElementsByClassName('confirm-button')[0];
-    const cancelButton: HTMLElement = container.getElementsByClassName('cancel-button')[0];
+    const span: HTMLElement = container.getElementsByTagName('span')[0];
+    const confirmButton: HTMLElement = container.getElementsByClassName('confirm')[0];
+    const cancelButton: HTMLElement = container.getElementsByClassName('cancel')[0];
 
-    h3.innerHTML = `Export ${ data.accountType.label }`; // public key for
+
+    // TODO: Check if data.accountType.label exists --> concrete type for data
+    span.innerText = data.accountType.label
 
     confirmButton.onclick = () => {
         postMessage(new UiMessage(UI.RECEIVE_CONFIRMATION, 'true'));
