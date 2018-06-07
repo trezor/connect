d9af155529be58388c5eb64c9b17eefc966d9405
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-07 12:34:52 +0200

    Add icon for unreadable device & add explanation

--- src/js/popup/view/selectDevice.js
+++ src/js/popup/view/selectDevice.js
@@ -59,20 +59,31 @@ export const selectDevice = (payload: $PropertyType<SelectDevice, 'payload'>): v
 
     // Populate device list
     const deviceList: HTMLElement = container.getElementsByClassName('select-device-list')[0];
-    deviceList.innerHTML = '';
+    // deviceList.innerHTML = '';
     const rememberCheckbox: HTMLInputElement = (container.getElementsByClassName('remember-device')[0]: any);
+
+    // Show readable devices first
+    payload.devices.sort((d1, d2) => {
+        if (d1.unreadable && !d2.unreadable) {
+            return 1;
+        } else if (!d1.unreadable && d2.unreadable) {
+            return -1;
+        }
+        return 0;
+    });
 
     payload.devices.forEach(device => {
         const deviceButton: HTMLButtonElement = document.createElement('button');
         deviceButton.className = 'list';
-
-        deviceButton.addEventListener('click', () => {
-            postMessage(new UiMessage(UI.RECEIVE_DEVICE, {
-                remember: (rememberCheckbox && rememberCheckbox.checked),
-                device
-            }));
-            showView('loader');
-        });
+        if (!device.unreadable) {
+            deviceButton.addEventListener('click', () => {
+                postMessage(new UiMessage(UI.RECEIVE_DEVICE, {
+                    remember: (rememberCheckbox && rememberCheckbox.checked),
+                    device
+                }));
+                showView('loader');
+            });
+        }
 
         const deviceIcon: HTMLSpanElement = document.createElement('span');
         deviceIcon.className = 'icon';
@@ -83,12 +94,28 @@ export const selectDevice = (payload: $PropertyType<SelectDevice, 'payload'>): v
             }
         }
 
+        // TODO: Different icon when device is unreadable (doesn't necessary need to be a TREZOR device)
         const deviceName: HTMLSpanElement = document.createElement('span');
         deviceName.className = 'device-name';
         deviceName.textContent = device.label;
 
-        deviceButton.appendChild(deviceIcon);
-        deviceButton.appendChild(deviceName);
+        const wrapper: HTMLDivElement = document.createElement('div');
+        wrapper.className = 'wrapper';
+        wrapper.appendChild(deviceIcon);
+        wrapper.appendChild(deviceName);
+        deviceButton.appendChild(wrapper);
+
+        if (device.unreadable) {
+            deviceButton.disabled = true;
+            deviceIcon.classList.add('unknown');
+
+            deviceButton.classList.add('device-explain');
+
+            const explanation: HTMLDivElement = document.createElement('div');
+            explanation.className = 'explain';
+            explanation.innerHTML = 'Please install <a href="https://wallet.trezor.io" target="_blank">Bridge</a> to use TREZOR ONE';
+            deviceButton.appendChild(explanation);
+        }
 
         deviceList.appendChild(deviceButton);
     });
