465b34d61e928f0cb05b78bd1a9f6932d6694570
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-07 18:04:34 +0200

    Add type to 'payload' parameter in 'initBrowserView'

--- src/js/popup/view/browser.js
+++ src/js/popup/view/browser.js
@@ -4,22 +4,27 @@
 import { UiMessage } from '../../core/CoreMessage';
 import * as UI from '../../constants/ui';
 import { container, showView, postMessage } from './common';
+import type { BrowserMessage } from 'flowtype/ui-message';
 
-export const initBrowserView = (data: Object): void => {
+export const initBrowserView = (payload: $PropertyType<BrowserMessage, 'payload'>): void => {
+    console.log(payload);
 
-    if (data.outdated) {
-        showView('browser-outdated');
+    showView('browser');
 
-        const button = container.getElementsByTagName('button')[0];
-        button.onclick = () => {
-            postMessage(new UiMessage(UI.RECEIVE_BROWSER));
-            showView('loader');
-        }
-    } else {
-        if (data.mobile) {
-            showView('browser-mobile');
-        } else {
-            showView('browser-not-supported');
-        }
+
+
+    const h3: HTMLElement = container.getElementsByTagName('h3')[0];
+    if (!payload.supported) {
+        h3.innerText = 'Browser is not supported';
+    } else if (payload.outdated) {
+        h3.innerText = 'Browser is outdated';
+    } else if (payload.mobile) {
+        // TODO: ???
     }
+
+    /* const button = container.getElementsByTagName('button')[0];
+    button.onclick = () => {
+        postMessage(new UiMessage(UI.RECEIVE_BROWSER));
+        showView('loader');
+    } */
 };
