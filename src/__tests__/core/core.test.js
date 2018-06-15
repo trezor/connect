d6eb30f436afa156694ad72159e128effac12d74
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-15 14:35:14 +0200

    Refactorize "getPublicKey", "ethereumGetAddress" and "ethereumSignMessage" to use "AbstractCoreEventHandler"
    Each test creates its own class of the handler that inherits from "AbstractCoreEventHandler".
    Then just
    - 1) override "handleResponseEvent" function
    - 2) pass core into a constructor
    - 3) call "startListening()"

--- src/__tests__/core/core.test.js
+++ src/__tests__/core/core.test.js
@@ -1,6 +1,3 @@
-//import { Core, CORE_EVENT, init as initCore, initTransport } from './src/js/core/Core.js';
-//import { parse as parseSettings } from '../entrypoints/ConnectSettings';
-
 import 'babel-polyfill';
 import { Core, init as initCore, initTransport } from '../../js/core/Core.js';
 import { checkBrowser } from '../../js/utils/browser';
@@ -10,6 +7,9 @@ import * as UI from '../../js/constants/ui';
 import { getPublicKeyTests } from './getPublicKey.spec.js';
 import { ethereumGetAddressTests } from './ethereumGetAddress.spec.js';
 import { ethereumSignMessageTests } from './ethereumSignMessage.spec.js';
+import { ethereumSignTxTests } from './ethereumSignTx.spec.js';
+import { ethereumVerifyMessageTests } from './ethereumVerifyMessage.spec.js';
+import { nemGetAddressTests } from './nemGetAddress.spec.js';
 
 // todo: generate this object automatically
 const testFunctions = {
@@ -21,6 +21,15 @@ const testFunctions = {
 const testToRun = __karma__.config.test;
 
 describe(`Testing method`, () => {
-    console.warn(testToRun);
+    let defaultTimeout;
+    beforeEach(() => {
+        defaultTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 250000;
+    });
+
+    afterEach(() => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = defaultTimeout;
+    });
+
     testFunctions[testToRun]();
 });
