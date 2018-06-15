8feafc548efbd32c1d1c443e85ef3a0ddee326a0
Szymon Lesisz <sz.lesisz@gmail.com>
2018-06-15 10:18:12 +0200

    moved type CoinInfo to 'flowtype' file

--- src/flowtype/index.js
+++ src/flowtype/index.js
@@ -31,6 +31,11 @@ declare var navigator: Navigator & {
 
 // Common types used across library
 declare module 'flowtype' {
+
+    import type {
+        Network as BitcoinJsNetwork,
+    } from 'bitcoinjs-lib-zcash';
+
     declare export type Deferred<T> = {
         id?: string,
         device: ?any,
@@ -59,4 +64,52 @@ declare module 'flowtype' {
         outdated: boolean;
         mobile: boolean;
     }
+
+    declare type CoinSupport = {
+        connect: boolean,
+        // "electrum": "https://electrum.org/",
+        trezor1: string,
+        trezor2: string,
+        // "webwallet": true
+    }
+
+    declare export type CoinInfo = {
+        addressPrefix: string,
+        // address_type in Network
+        // address_type_p2sh in Network
+        bitcore: Array<string>,
+        blockbook: Array<string>,
+        blocktime: number,
+        cashAddrPrefix: ?string,
+        label: string,
+        name: string,
+        shortcut: string,
+        curveName: string,
+        decred: boolean,
+        defaultFees: {[level: string]: number},
+        dustLimit: number,
+        forceBip143: boolean,
+        forkid: ?number,
+        hashGenesisBlock: string,
+        maxAddressLength: number,
+        maxFeeSatoshiKb: number,
+        minAddressLength: number,
+        minFeeSatoshiKb: number,
+        segwit: boolean,
+        slip44: number,
+        support: CoinSupport,
+        xPubMagic: string,
+        xPubMagicSegwit: ?string,
+        xPubMagicSegwitNative: ?string,
+
+        network: BitcoinJsNetwork,
+        zcash: boolean,
+        isBitcoin: boolean,
+        hasSegwit: boolean,
+        minFee: number,
+        maxFee: number,
+        blocktime: number,
+        // used in backend
+        blocks?: number,
+    };
 }
