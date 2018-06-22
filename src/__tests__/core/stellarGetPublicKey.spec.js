625a761deeddfd5ad02051d07b39e59f4328ab1b
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-22 18:04:39 +0200

    Prepare tests for 'StellarGetPublicKey'  and 'StellarSignTransaction'

---
+++ src/__tests__/core/stellarGetPublicKey.spec.js
@@ -0,0 +1,49 @@
+import { Core, init as initCore, initTransport } from '../../js/core/Core.js';
+import { checkBrowser } from '../../js/utils/browser';
+import { settings, CoreEventHandler } from './common.js';
+
+export const stellarGetPublicKeyTests = (): void => {
+    describe('StellarGetPublicKey', () => {
+        let core: Core;
+
+        beforeEach(async (done) => {
+            core = await initCore(settings);
+            checkBrowser();
+            done();
+        });
+        afterEach(() => {
+            // Deinitialize existing core
+            core.onBeforeUnload();
+        });
+
+        const testPayloads = [
+            {
+                method: 'stellarGetPublicKey',
+                path: 'm/44h/148h/0h',
+            },
+        ];
+        const expectedResponses = [
+            {
+                payload: {
+                    todo: 'check python'
+                    // assert stellar.address_from_public_key(response.public_key) == b'GAK5MSF74TJW6GLM7NLTL76YZJKM2S4CGP3UH4REJHPHZ4YBZW2GSBPW'
+                },
+            },
+        ];
+
+        if (testPayloads.length !== expectedResponses.length) {
+            throw new Error('Different number of payloads and expected responses');
+        }
+
+        for (let i = 0; i < testPayloads.length; i++) {
+            const payload = testPayloads[i];
+            const expectedResponse = expectedResponses[i];
+
+            it('', async (done) => {
+                const handler = new CoreEventHandler(core, payload, expectedResponse, expect, done);
+                handler.startListening();
+                await initTransport(settings);
+            });
+        }
+    });
+};
---
+++ src/__tests__/core/stellarSignTransaction.spec.js
@@ -0,0 +1,40 @@
+import { Core, init as initCore, initTransport } from '../../js/core/Core.js';
+import { checkBrowser } from '../../js/utils/browser';
+import { settings, CoreEventHandler } from './common.js';
+
+export const stellarSignTransactionTests = (): void => {
+    const subtest = __karma__.config.subtest;
+    const availableSubtests = {
+
+    };
+
+    describe('StellarSignTransaction', () => {
+        let core: Core;
+
+        beforeEach(async (done) => {
+            core = await initCore(settings);
+            checkBrowser();
+            done();
+        });
+        afterEach(() => {
+            // Deinitialize existing core
+            core.onBeforeUnload();
+        });
+
+        const { testPayloads, expectedResponses, specName } = availableSubtests[subtest]();
+        if (testPayloads.length !== expectedResponses.length) {
+            throw new Error('Different number of payloads and expected responses');
+        }
+
+        for (let i = 0; i < testPayloads.length; i++) {
+            const payload = testPayloads[i];
+            const expectedResponse = expectedResponses[i];
+
+            it(specName, async (done) => {
+                const handler = new CoreEventHandler(core, payload, expectedResponse, expect, done);
+                handler.startListening();
+                await initTransport(settings);
+            });
+        }
+    });
+};
