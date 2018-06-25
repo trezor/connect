25b1cd7e5b541517d07766f8ae38eacf0c978bc5
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-25 10:23:31 +0200

    Remove 'VerifyMessage' (segwit-native) test for different UTF encodings

--- src/__tests__/core/verifyMessageSegwitNative.spec.js
+++ src/__tests__/core/verifyMessageSegwitNative.spec.js
@@ -87,42 +87,12 @@ const verifyTestnetSubtest = (): void => {
     };
 };
 
-const verifyUtfSubtest = (): void => {
-    const testPayloads = [
-        {
-            method: 'verifyMessage',
-            coin: 'Bitcoin',
-            address: 'bc1qyjjkmdpu7metqt5r36jf872a34syws33s82q2j',
-            signature: '28d0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda41c6a771b13d495dd225b13b0a9d0f915a984ee3d0703f92287bf8009fbb9f7d6',
-            message: 'Pr\u030ci\u0301s\u030cerne\u030c z\u030clut\u030couc\u030cky\u0301 ku\u030an\u030c u\u0301pe\u030cl d\u030ca\u0301belske\u0301 o\u0301dy za\u0301ker\u030cny\u0301 uc\u030cen\u030c be\u030cz\u030ci\u0301 pode\u0301l zo\u0301ny u\u0301lu\u030a',
-        },
-        {
-            method: 'verifyMessage',
-            coin: 'Bitcoin',
-            address: 'bc1qyjjkmdpu7metqt5r36jf872a34syws33s82q2j',
-            signature: '28d0ec02ed8da8df23e7fe9e680e7867cc290312fe1c970749d8306ddad1a1eda41c6a771b13d495dd225b13b0a9d0f915a984ee3d0703f92287bf8009fbb9f7d6',
-            message: 'P\u0159\xed\u0161ern\u011b \u017elu\u0165ou\u010dk\xfd k\u016f\u0148 \xfap\u011bl \u010f\xe1belsk\xe9 \xf3dy z\xe1ke\u0159n\xfd u\u010de\u0148 b\u011b\u017e\xed pod\xe9l z\xf3ny \xfal\u016f',
-        },
-    ];
-    const expectedResponses = [
-        { success: true },
-        { success: true },
-    ];
-
-    return {
-        testPayloads,
-        expectedResponses,
-        specName: '/verifyUtf',
-    };
-};
-
 export const verifyMessageSegwitNativeTests = (): void => {
     const subtest = __karma__.config.subtest;
     const availableSubtests = {
         verify: verifySubtest,
         verifyLong: verifyLongSubtest,
         verifyTestnet: verifyTestnetSubtest,
-        verifyUtf: verifyUtfSubtest,
     };
 
     describe('verifyMessageSegwitNative', () => {
