cf723dc8ef234249eea8181a82bebfc3f65e4d78
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-26 13:14:24 +0200

    Add flowtypes for jasmine and karma

---
+++ src/flowtype/npm/jasmine.js
@@ -0,0 +1,22 @@
+/* @flow */
+
+declare module 'flowtype/jasmine' {
+    declare type DoneFn = () => void;
+    declare type ActionFn = (done: DoneFn) => Promise<void> | void;
+    declare type AssertionFn = (done: DoneFn) => Promise<void> | void;
+
+    declare export var jasmine: any;
+
+    declare export function describe(description: string, specDef?: () => void): void;
+
+    declare export function beforeAll(action: ActionFn, timeout?: number): void;
+    declare export function beforeEach(action: ActionFn, timeout?: number): void;
+
+    declare export function afterAll(action: ActionFn, timeout?: number): void;
+    declare export function afterEach(action: ActionFn, timeout?: number): void;
+
+    declare export function it(expectation: string, assertion?: AssertionFn, timeout?: number): void;
+
+    // todo?
+    declare export function expect(): Function;
+}
---
+++ src/flowtype/npm/karma.js
@@ -0,0 +1,5 @@
+/* @flow */
+
+declare module 'flowtype/karma' {
+    declare export var __karma__: any;
+}
