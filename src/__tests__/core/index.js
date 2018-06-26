8ee185aa815389badf27d7d3ce8c26df265b7345
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-26 09:29:06 +0200

    Refactorize test function imports
    Move all test function imports into a single file

---
+++ src/__tests__/core/index.js
@@ -0,0 +1,22 @@
+export { getPublicKey } from './getPublicKey.spec.js';
+export { getAddress } from './getAddress.spec.js';
+export { getAddressSegwit } from './getAddressSegwit.spec.js';
+export { signMessage } from './signMessage.spec.js';
+export { signMessageSegwit } from './signMessageSegwit.spec.js';
+export { signTx } from './signTx.spec.js';
+export { signTxSegwit } from './signTxSegwit.spec.js';
+export { signTxBgold } from './signTxBgold.spec.js';
+export { signTxBcash } from './signTxBcash.spec.js';
+export { verifyMessage } from './verifyMessage.spec.js';
+export { verifyMessageSegwit } from './verifyMessageSegwit.spec.js';
+/* export { verifyMessageSegwitNative } from './verifyMessageSegwitNative.spec.js'; */
+export { ethereumGetAddress } from './ethereumGetAddress.spec.js';
+export { ethereumSignMessage } from './ethereumSignMessage.spec.js';
+export { ethereumSignTx } from './ethereumSignTx.spec.js';
+export { ethereumVerifyMessage } from './ethereumVerifyMessage.spec.js';
+export { nemGetAddress } from './nemGetAddress.spec.js';
+/* export { nemSignTransactionMosaicTests } from './nemSignTransactionMosaic.spec.js';
+export { nemSignTransactionMultisigTests } from './nemSignTransactionMultisig.spec.js';
+export { nemSignTransactionOthersTests } from './nemSignTransactionOthers.spec.js';
+export { nemSignTransactionTransfersTests } from './nemSignTransactionTransfers.spec.js'; */
+/* export { stellarGetPublicKeyTests } from './stellarGetPublicKey.spec.js'; */
