02dda3d78ea0fd9f0b1f3283abb4cf1c0203ee2d
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-27 13:01:16 +0200

    Add Flow types for 'EthereumSignTx' tests

--- src/flowtype/tests/ethereum-sign-tx.js
+++ src/flowtype/tests/ethereum-sign-tx.js
@@ -1,7 +1,23 @@
 /* @flow */
 
 declare module 'flowtype/tests/ethereum-sign-tx' {
-    import type { Transaction } from 'flowtype/Ethereum'
+     // TODO: Use type Trans from 'flowtype/Ethereum' instead
+     // current problem is that I'm not sure whether rvs values are optional
+     // because they shouldn't be needed when signing tx
+     // they should be returned *after* signing the tx
+     // import type { Transaction } from 'flowtype/Ethereum'
+    declare type Transaction = {
+        to: string,
+        value: string,
+        gasPrice: string,
+        gasLimit: string,
+        nonce: string,
+        data?: string,
+        chainId?: number,
+        v?: string,
+        r?: string,
+        s?: string,
+    }
 
     declare export type TestEthereumSignTxPayload = {
         method: string,
@@ -15,11 +31,5 @@ declare module 'flowtype/tests/ethereum-sign-tx' {
             s: string,
             v?: number
         },
-    };
-
-    declare export type SubtestEthereumSignTx = {
-        testPayloads: Array<TestEthereumSignTxPayload>,
-        expectedResponses: Array<ExpectedEthereumSignTxResponse>,
-        specName: string,
     };
 }
