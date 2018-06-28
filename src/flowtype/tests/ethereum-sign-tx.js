603dddd6dba96834e5cba9bb78e1207f70fd80f9
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-06-28 10:16:46 +0200

    Use already defined type for an ethereum transaction

--- src/flowtype/tests/ethereum-sign-tx.js
+++ src/flowtype/tests/ethereum-sign-tx.js
@@ -1,28 +1,11 @@
 /* @flow */
 
+import type { Transaction as EthereumTransaction } from 'flowtype/Ethereum';
 declare module 'flowtype/tests/ethereum-sign-tx' {
-     // TODO: Use type Trans from 'flowtype/Ethereum' instead
-     // current problem is that I'm not sure whether rvs values are optional
-     // because they shouldn't be needed when signing tx
-     // they should be returned *after* signing the tx
-     // import type { Transaction } from 'flowtype/Ethereum'
-    declare type Transaction = {
-        to: string,
-        value: string,
-        gasPrice: string,
-        gasLimit: string,
-        nonce: string,
-        data?: string,
-        chainId?: number,
-        v?: string,
-        r?: string,
-        s?: string,
-    }
-
     declare export type TestEthereumSignTxPayload = {
         method: string,
         path: string | Array<number>,
-        transaction: Transaction,
+        transaction: EthereumTransaction,
     };
     declare export type ExpectedEthereumSignTxResponse = {
         success?: boolean,
