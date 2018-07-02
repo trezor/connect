0f2883506a62783125893996839e61e3623bfc95
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-07-02 13:25:37 +0200

    Implement signTxMultisigChange

--- src/flowtype/tests/index.js
+++ src/flowtype/tests/index.js
@@ -69,7 +69,7 @@ declare module 'flowtype/tests' {
     declare export type SignTxBgoldAvailableSubtests = 'change' | 'noChange' | 'p2sh' | 'p2shWitnessChange';
     declare export type SignTxBcashAvailableSubtests = 'change' | 'noChange' | 'oldAddr';
     declare export type SignTxMultisigAvailableSubtests = 'twoOfThree' | 'fifteenOfFifteen' | 'missingPubkey';
-    declare export type SignTxMultisigChangeAvailableSubtests = 'externalExternal';
+    declare export type SignTxMultisigChangeAvailableSubtests = 'externalExternal' | 'externalInternal' | 'internalExternal' | 'multisigExternalExternal';
     declare export type VerifyMessageAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet' | 'verifyBcash' | 'verifyBitcoind';
     declare export type VerifyMessageSegwitAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
     declare export type VerifyMessageSegwitNativeAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
