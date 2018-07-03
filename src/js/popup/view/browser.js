104c43567d1961de86e9302acb9f6ed62766aad6
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-03 16:59:39 +0200

    fixed unsupported browser view (mobile/desktop)

--- src/js/popup/view/browser.js
+++ src/js/popup/view/browser.js
@@ -7,27 +7,12 @@ import { container, showView, postMessage } from './common';
 import type { BrowserMessage } from 'flowtype/ui-message';
 
 export const initBrowserView = (payload: $PropertyType<BrowserMessage, 'payload'>): void => {
-    console.log(payload);
-
-    showView('browser');
-
-
+    showView(!payload.supported && payload.mobile ? 'smartphones-not-supported' : 'browser');
 
     const h3: HTMLElement = container.getElementsByTagName('h3')[0];
-    const p: HTMLElement = container.getElementsByTagName('p')[0];
-    if (!payload.supported) {
-        h3.innerText = 'Unsupported browser';
-        p.innerText = 'Please use one of the supported browsers.';
+    if (!payload.supported && !payload.mobile) {
+        h3.innerText = 'Browser is not supported';
     } else if (payload.outdated) {
-        h3.innerText = 'Outdated browser';
-        p.innerText = 'Please use update your browser.';
-    } else if (payload.mobile) {
-        // TODO: ???
+        h3.innerText = 'Browser is outdated';
     }
-
-    /* const button = container.getElementsByTagName('button')[0];
-    button.onclick = () => {
-        postMessage(new UiMessage(UI.RECEIVE_BROWSER));
-        showView('loader');
-    } */
 };
