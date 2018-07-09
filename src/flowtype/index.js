5edcfa50f1ad092b8496a3e13beec430f3aad33c
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-09 15:18:54 +0200

    removed "trezor-connect" flowtype module

--- src/flowtype/index.js
+++ src/flowtype/index.js
@@ -5,25 +5,6 @@ declare module 'CSSModule' {
     declare var exports: { [key: string]: string };
 }
 
-// this needs to be toplevel
-// declare type $Core$Message = {
-//     +event: string;
-//     +type: string;
-//     +payload: any;
-
-//     id?: number; // response id in ResponseMessage
-//     success?: boolean; // response status in ResponseMessage
-// }
-
-// Override MessageEvent to have access to "ports" field and typed "data"
-// declare class Message extends Event {
-//     +origin: string;
-//     +lastEventId: string;
-//     +source: WindowProxy;
-//     +ports: Array<MessagePort>;
-//     +data: ?$Core$Message;
-// }
-
 // Override Navigator to have access to "usb" field
 declare var navigator: Navigator & {
     +usb?: USB;
@@ -36,26 +17,10 @@ declare module 'flowtype' {
         Network as BitcoinJsNetwork,
     } from 'bitcoinjs-lib-zcash';
 
-    // declare export type Deferred<T> = {
-    //     id?: string,
-    //     device: ?any,
-    //     promise: Promise<T>,
-    //     resolve: (t: T) => void,
-    //     reject: (e: Error) => void,
-    // };
-
     declare export type UiPromiseResponse = {
         event: string,
         payload: any,
     }
-
-    declare export type PostMessageData = {
-        event: string,
-        type: string,
-        payload?: any
-    }
-
-    // declare export type CoreMessage = $Core$Message;
 
     declare export type BrowserState = {
         name: string;
