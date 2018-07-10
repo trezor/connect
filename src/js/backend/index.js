0eaed0ac269a14f2157a734225d4f3564d4f5899
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-10 12:17:01 +0200

    removed depreciated method for backend creation

--- src/js/backend/index.js
+++ src/js/backend/index.js
@@ -3,10 +3,9 @@
 
 import BlockBook from './BlockBook';
 
-import { getCoinInfoByHash, getCoinInfoByCurrency } from '../data/CoinInfo';
 import type { CoinInfo } from 'flowtype';
 
-const instances: Array<any> = [];
+const instances: Array<BlockBook> = [];
 
 export const find = (urls: Array<string>): ?BlockBook => {
     for (let i: number = 0; i < instances.length; i++) {
@@ -17,58 +16,29 @@ export const find = (urls: Array<string>): ?BlockBook => {
     return null;
 };
 
-export const createFromCurrency = async (currency: string): Promise<BlockBook> => {
-    const coinInfo: ?CoinInfo = getCoinInfoByCurrency(currency);
-    if (!coinInfo) {
-        throw new Error('Currency not found for ' + currency);
+export const remove = (backend: BlockBook): void => {
+    const index: number = instances.indexOf(backend);
+    if (index >= 0) {
+        instances.splice(index, 1);
     }
-    // get urls from coins.json using currency name/shortcut
-    if (coinInfo.blockbook.length < 1) {
-        throw new Error('Blockbook urls not found for ' + currency);
-    }
-
-    let backend: ?BlockBook = find(coinInfo.blockbook);
-    if (!backend) {
-        backend = new BlockBook({ urls: coinInfo.blockbook, coinInfo: coinInfo });
-        instances.push(backend);
-    }
-    await backend.loadCoinInfo(coinInfo);
-    // instances.push(backend);
-    return backend;
-};
+}
 
 export const createFromCoinInfo = async (coinInfo: CoinInfo): Promise<BlockBook> => {
     let backend: ?BlockBook = find(coinInfo.blockbook);
     if (!backend) {
-        backend = new BlockBook({ urls: coinInfo.blockbook, coinInfo: coinInfo });
+        backend = new BlockBook({ urls: coinInfo.blockbook, coinInfo });
         instances.push(backend);
     }
-    // await backend.loadCoinInfo();
+    try {
+        await backend.loadCoinInfo(coinInfo);
+    } catch (error) {
+        remove(backend);
+        throw error;
+    }
     return backend;
 };
-
-// CoinInfo will be find by network hash
-export const createFromUrl = async (urls: Array<string>): Promise<BlockBook> => {
-    let backend: ?BlockBook = find(urls);
-    if (!backend) {
-        backend = new BlockBook({ urls: urls });
-        instances.push(backend);
-    }
-    await backend.loadCoinInfo();
-    return backend;
+export const create = async (coinInfo: CoinInfo): Promise<BlockBook> => {
+    return await createFromCoinInfo(coinInfo);
 };
-
-export const create = async (urlsOrCurrency: CoinInfo | Array<string> | string): Promise<BlockBook> => {
-    if (Array.isArray(urlsOrCurrency)) {
-        return await createFromUrl(urlsOrCurrency);
-    } else if (typeof urlsOrCurrency === 'object') {
-        return await createFromCoinInfo(urlsOrCurrency);
-    } else if (typeof urlsOrCurrency === 'string') {
-        return await createFromCurrency(urlsOrCurrency);
-    } else {
-        throw new Error('Invalid params ' + urlsOrCurrency);
-    }
-};
-
 
 export default BlockBook;
