20d55d490daf9595e2186145687e1bd8b307404f
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-17 18:19:38 +0200

    EthereumGetAddress with bundled params

--- src/js/core/methods/EthereumGetAddress.js
+++ src/js/core/methods/EthereumGetAddress.js
@@ -4,12 +4,21 @@
 import AbstractMethod from './AbstractMethod';
 import { validateParams, validateEthereumPath } from './helpers/paramsValidator';
 import { validatePath } from '../../utils/pathUtils';
+
+import * as UI from '../../constants/ui';
+import { UiMessage } from '../../message/builder';
+
 import type { EthereumAddress } from '../../types/trezor';
 import type { CoreMessage } from '../../types';
 
-type Params = {
+type Batch = {
     path: Array<number>;
     showOnTrezor: boolean;
+}
+
+type Params = {
+    bundle: Array<Batch>;
+    bundledResponse: boolean;
 }
 
 export default class EthereumGetAddress extends AbstractMethod {
@@ -24,33 +33,71 @@ export default class EthereumGetAddress extends AbstractMethod {
         this.info = 'Export Ethereum address';
 
         const payload: Object = message.payload;
-
-        // validate incoming parameters
-        validateParams(payload, [
-            { name: 'path', obligatory: true },
-            { name: 'showOnTrezor', type: 'boolean' },
-        ]);
-
-        const path: Array<number> = validatePath(payload.path);
-        validateEthereumPath(path);
-
-        let showOnTrezor: boolean = true;
-        if (payload.hasOwnProperty('showOnTrezor')){
-            showOnTrezor = payload.showOnTrezor;
+        let bundledResponse: boolean = true;
+        // create a bundle with only one batch
+        if (!payload.hasOwnProperty('bundle')) {
+            payload.bundle = [ ...payload ];
+            bundledResponse = false;
         }
 
-        this.useUi = showOnTrezor;
+        // validate bundle type
+        validateParams(payload, [
+            { name: 'bundle', type: 'array' },
+        ]);
+
+        const bundle = [];
+        let shouldUseUi: boolean = false;
+        payload.bundle.forEach(batch => {
+            // validate incoming parameters for each batch
+            validateParams(batch, [
+                { name: 'path', obligatory: true },
+                { name: 'showOnTrezor', type: 'boolean' },
+            ]);
+
+            const path: Array<number> = validatePath(batch.path);
+            validateEthereumPath(path);
+
+            let showOnTrezor: boolean = true;
+            if (batch.hasOwnProperty('showOnTrezor')){
+                showOnTrezor = batch.showOnTrezor;
+            }
+            if (showOnTrezor) {
+                shouldUseUi = true;
+            }
+
+            bundle.push({
+                path,
+                showOnTrezor
+            });
+        });
+
+        this.useUi = shouldUseUi;
 
         this.params = {
-            path,
-            showOnTrezor
+            bundle,
+            bundledResponse
         }
     }
 
-    async run(): Promise<EthereumAddress> {
-        return await this.device.getCommands().ethereumGetAddress(
-            this.params.path,
-            this.params.showOnTrezor
-        );
+    async run(): Promise<EthereumAddress | Array<EthereumAddress>> {
+
+        const responses: Array<EthereumAddress> = [];
+        for (let i = 0; i < this.params.bundle.length; i++) {
+
+            const response = await this.device.getCommands().ethereumGetAddress(
+                this.params.bundle[i].path,
+                this.params.bundle[i].showOnTrezor
+            );
+            responses.push(response);
+
+            if (this.params.bundledResponse) {
+                // send progress
+                this.postMessage(new UiMessage(UI.BUNDLE_PROGRESS, {
+                    progress: i,
+                    response
+                }));
+            }
+        }
+        return this.params.bundledResponse ? responses : responses[0];
     }
 }
