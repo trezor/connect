a4af23e8fe3189ddc9111eb900150014af6e3a45
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-07-20 14:46:43 +0200

    FIx tests after rebase

--- src/__tests__/core/ethereumSignMessage.spec.js
+++ src/__tests__/core/ethereumSignMessage.spec.js
@@ -28,13 +28,13 @@ export const ethereumSignMessage = () => {
         const expectedResponses: Array<ExpectedEthereumSignMessageResponse> = [
             {
                 payload: {
-                    address: 'ae2b111b634f8fb3942b13b98c824b0f1060cacb',
+                    address: '0xAE2B111b634f8FB3942B13b98c824B0F1060cacB',
                     signature: '9dc221f51fe1515d598324e51c1897637c3f0098b02758fa68a794803495df061ddbc1460b40497a80f3b1bf63e5966850c3bbd8ff91e0e9f4d7a121be32b1c21c',
                 },
             },
             {
                 payload: {
-                    address: 'ae2b111b634f8fb3942b13b98c824b0f1060cacb',
+                    address: '0xAE2B111b634f8FB3942B13b98c824B0F1060cacB',
                     signature: '076d6dcf1fdcac60748919a781dc39d0ee7beece5fd5604be2f061e32ec6d91817562df343171ab38214fa3e2de46240ab330da34011856e2461378940243ec91c',
                 },
             },
