171c08180fbe22c209ffc77675d9cbe23d142a0f
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-24 18:12:25 +0200

    Refactoring constants

--- src/js/constants/iframe.js
+++ src/js/constants/iframe.js
@@ -1,6 +1,6 @@
 /* @flow */
 'use strict';
 
-export const ERROR: 'iframe_error' = 'iframe_error';
-export const CALL: 'iframe_call' = 'iframe_call';
-export const RESPONSE: 'iframe_response' = 'iframe_response';
+export const ERROR: 'iframe-error' = 'iframe-error';
+export const CALL: 'iframe-call' = 'iframe-call';
+export const RESPONSE: 'iframe-response' = 'iframe-response';
