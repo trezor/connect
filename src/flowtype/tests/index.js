3e151e96b67a1d8f069ab80e726743b38177a44a
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-07-25 23:11:20 +0200

    Comment unnecessary flowtypes

--- src/flowtype/tests/index.js
+++ src/flowtype/tests/index.js
@@ -63,39 +63,39 @@ import type {
 
 
 declare module 'flowtype/tests' {
-    declare export type GetAddressAvailableSubtests = 'btc' | 'ltc' | 'tbtc' | 'bch';
-    declare export type GetAddressSegwitAvailableSubtests = 'showSegwit';
-    declare export type SignMessageAvailableSubtests = 'sign' | 'signTestnet' | 'signBch' | 'signLong';
-    declare export type SignMessageSegwitAvailableSubtests = 'sign' | 'signLong';
-    declare export type SignTransactionAvailableSubtests =
-        'oneOneFee'
-        | 'oneTwoFee'
-        | 'oneThreeFee'
-        | 'twoTwo'
-        | 'testnetOneTwoFee'
-        | 'testnetFeeTooHigh'
-        | 'lotsOfOutputs'
-        | 'feeTooHigh'
-        | 'notEnoughFunds'
-        | 'spendCoinbase'
-        | 'twoChanges'
-        | 'p2sh'
-        | 'changeOnMainChainAllowed';
-    declare export type SignTransactionSegwitAvailableSubtests = 'sendP2sh' | 'sendP2shChange' | 'sendMultisig1';
-    declare export type SignTransactionBgoldAvailableSubtests = 'change' | 'noChange' | 'p2sh' | 'p2shWitnessChange' | 'sendMultisig1';
-    declare export type SignTransactionBcashAvailableSubtests = 'change' | 'noChange' | 'oldAddr';
-    declare export type SignTransactionMultisigAvailableSubtests = 'twoOfThree' | 'fifteenOfFifteen' | 'missingPubkey';
-    declare export type SignTransactionMultisigChangeAvailableSubtests = 'externalExternal' | 'externalInternal' | 'internalExternal' | 'multisigExternalExternal';
-    declare export type VerifyMessageAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet' | 'verifyBcash' | 'verifyBitcoind';
-    declare export type VerifyMessageSegwitAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
-    declare export type VerifyMessageSegwitNativeAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
-    declare export type EthereumSignTransactionAvailableSubtests = 'knownErc20Token' | 'unknownErc20Token' | 'noData' | 'data' | 'message' | 'newContract' | 'sanityChecks' | 'noDataEip155' | 'dataEip155';
-    declare export type NemSignTransactionMosaicAvailableSubtests = 'supplyChange' | 'creation' | 'creationProperties' | 'creationLevy';
-    declare export type NemSignTransactionMultisigAvailableSubtests = 'aggregateModification' | 'multisig' | 'multisigSigner';
-    declare export type NemSignTransactionOthersAvailableSubtests = 'importanceTransfer' | 'provisionNamespace';
-    declare export type NemSignTransactionTransfersAvailableSubtests = 'simple' | 'encryptedPayload' | 'xemAsMosaic' | 'unknownMosaic' | 'knownMosaic' | 'knownMosaicWithLevy' | 'multipleMosaics';
-    declare export type GetAccountInfoAvailableSubtests = 'firstAccount' | 'zeroBalance' | 'pathInvalid' | 'zeroBalance' | 'xpubInsteadOfPath';
-    declare export type PassphraseAvailableSubtests = 'correctPassphrase' | 'wrongPassphrase';
+    // declare export type GetAddressAvailableSubtests = 'btc' | 'ltc' | 'tbtc' | 'bch';
+    // declare export type GetAddressSegwitAvailableSubtests = 'showSegwit';
+    // declare export type SignMessageAvailableSubtests = 'sign' | 'signTestnet' | 'signBch' | 'signLong';
+    // declare export type SignMessageSegwitAvailableSubtests = 'sign' | 'signLong';
+    // declare export type SignTransactionAvailableSubtests =
+    //     'oneOneFee'
+    //     | 'oneTwoFee'
+    //     | 'oneThreeFee'
+    //     | 'twoTwo'
+    //     | 'testnetOneTwoFee'
+    //     | 'testnetFeeTooHigh'
+    //     | 'lotsOfOutputs'
+    //     | 'feeTooHigh'
+    //     | 'notEnoughFunds'
+    //     | 'spendCoinbase'
+    //     | 'twoChanges'
+    //     | 'p2sh'
+    //     | 'changeOnMainChainAllowed';
+    // declare export type SignTransactionSegwitAvailableSubtests = 'sendP2sh' | 'sendP2shChange' | 'sendMultisig1';
+    // declare export type SignTransactionBgoldAvailableSubtests = 'change' | 'noChange' | 'p2sh' | 'p2shWitnessChange' | 'sendMultisig1';
+    // declare export type SignTransactionBcashAvailableSubtests = 'change' | 'noChange' | 'oldAddr';
+    // declare export type SignTransactionMultisigAvailableSubtests = 'twoOfThree' | 'fifteenOfFifteen' | 'missingPubkey';
+    // declare export type SignTransactionMultisigChangeAvailableSubtests = 'externalExternal' | 'externalInternal' | 'internalExternal' | 'multisigExternalExternal';
+    // declare export type VerifyMessageAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet' | 'verifyBcash' | 'verifyBitcoind';
+    // declare export type VerifyMessageSegwitAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
+    // declare export type VerifyMessageSegwitNativeAvailableSubtests = 'verify' | 'verifyLong' | 'verifyTestnet';
+    // declare export type EthereumSignTransactionAvailableSubtests = 'knownErc20Token' | 'unknownErc20Token' | 'noData' | 'data' | 'message' | 'newContract' | 'sanityChecks' | 'noDataEip155' | 'dataEip155';
+    // declare export type NemSignTransactionMosaicAvailableSubtests = 'supplyChange' | 'creation' | 'creationProperties' | 'creationLevy';
+    // declare export type NemSignTransactionMultisigAvailableSubtests = 'aggregateModification' | 'multisig' | 'multisigSigner';
+    // declare export type NemSignTransactionOthersAvailableSubtests = 'importanceTransfer' | 'provisionNamespace';
+    // declare export type NemSignTransactionTransfersAvailableSubtests = 'simple' | 'encryptedPayload' | 'xemAsMosaic' | 'unknownMosaic' | 'knownMosaic' | 'knownMosaicWithLevy' | 'multipleMosaics';
+    // declare export type GetAccountInfoAvailableSubtests = 'firstAccount' | 'zeroBalance' | 'pathInvalid' | 'zeroBalance' | 'xpubInsteadOfPath';
+    // declare export type PassphraseAvailableSubtests = 'correctPassphrase' | 'wrongPassphrase';
 
     declare type Subtest<T, R> = {
         testPayloads: Array<T>,
