bc90ec5b1b935734e77959944c2e2833f8db4de2
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-26 12:06:07 +0200

    do not perform Initialize on excluded device

--- src/js/device/DeviceList.js
+++ src/js/device/DeviceList.js
@@ -209,10 +209,6 @@ export default class DeviceList extends EventEmitter {
     async _createAndSaveDevice(
         descriptor: DeviceDescriptor
     ): Promise<void> {
-        if (DataManager.isExcludedDevice(descriptor.path)) {
-            this.resolveTransportEvent();
-            return;
-        }
         _log.debug('Creating Device', descriptor);
         await new CreateDeviceHandler(descriptor, this).handle();
     }
@@ -398,7 +394,8 @@ class CreateDeviceHandler {
     async _takeAndCreateDevice(): Promise<void> {
         const device = await Device.fromDescriptor(this.list.transport, this.descriptor);
         this.list.devices[this.path] = device;
-        await device.run();
+        if (!DataManager.isExcludedDevice(this.path))
+            await device.run();
         this.list.emit(DEVICE.CONNECT, device.toMessageObject());
     }
 
