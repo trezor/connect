3cb784a1ca6f789e86f1813178b33a11c9d8bfef
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-31 15:57:18 +0200

    constants for webextensions

--- src/js/constants/popup.js
+++ src/js/constants/popup.js
@@ -2,7 +2,8 @@
 'use strict';
 
 export const INIT: 'popup-init' = 'popup-init';
-export const BOOTSTRAP: 'popup-bootstrap' = 'popup-bootstrap';
+export const EXTENSION_REQUEST: 'popup-extension_request' = 'popup-extension_request';
+export const EXTENSION_USB_PERMISSIONS: 'open-usb-permissions' = 'open-usb-permissions';
 export const LOG: 'popup-log' = 'popup-log';
 export const OPENED: 'popup-opened' = 'popup-opened';
 export const OPEN_TIMEOUT: 'popup-open_timeout' = 'popup-open_timeout';
