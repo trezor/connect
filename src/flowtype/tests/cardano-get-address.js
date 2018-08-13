ae0da5f90b131cef47fcc44ced0bd225bf0dc375
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-08-13 15:49:35 +0200

    Add tests for Cardano

---
+++ src/flowtype/tests/cardano-get-address.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+declare module 'flowtype/tests/cardano-get-address' {
+    declare export type TestCardanoGetAddressPayload = {
+        method: string,
+        path: string | Array<number>,
+    };
+
+    declare export type ExpectedCardanoGetAddressResponse = {
+        success?: boolean,
+        payload?: {
+            address: string,
+            publicKey: string,
+        },
+    };
+}
