812c10bfb241bedce06ae4e6c725ae98cbff1afc
Szymon Lesisz <sz.lesisz@gmail.com>
2018-08-23 15:40:46 +0200

    eslint fix

--- src/js/types/cardano.js
+++ src/js/types/cardano.js
@@ -6,7 +6,7 @@ import type { $Path, $Common } from './params';
 import type { Unsuccessful$ } from './response';
 import type {
     Success,
-    HDPubNode
+    HDPubNode,
 } from './trezor';
 
 // GetAddress
