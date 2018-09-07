a908e6c510e84111f59fb185f61c91c9dcaf3557
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-07 15:15:13 +0200

    new type of message "BlockchainMessage"

---
+++ src/js/constants/blockchain.js
@@ -0,0 +1,10 @@
+/* @flow */
+'use strict';
+
+// device list events
+export const INIT: 'blockchain-init' = 'blockchain-init';
+export const START: 'blockchain-start' = 'blockchain-start';
+export const ERROR: 'blockchain-error' = 'blockchain-error';
+export const CONNECT: 'blockchain-connect' = 'blockchain-connect';
+export const BLOCK: 'blockchain-block' = 'blockchain-block';
+export const ADDRESS: 'blockchain-address' = 'blockchain-address';
