bd7144f06b305f3f8cb129728a03e7905668f0dd
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-18 09:51:38 +0200

    remove unused BLOCKCHAIN constants

--- src/js/constants/blockchain.js
+++ src/js/constants/blockchain.js
@@ -1,11 +1,8 @@
 /* @flow */
 'use strict';
 
-// device list events
-export const INIT: 'blockchain-init' = 'blockchain-init';
-export const START: 'blockchain-start' = 'blockchain-start';
+// blockchain events
 export const ERROR: 'blockchain-error' = 'blockchain-error';
 export const CONNECT: 'blockchain-connect' = 'blockchain-connect';
 export const BLOCK: 'blockchain-block' = 'blockchain-block';
-export const ADDRESS: 'blockchain-address' = 'blockchain-address';
 export const NOTIFICATION: 'blockchain-notification' = 'blockchain-notification';
