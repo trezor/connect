58815e428bc8fe0ab81bfba955a5af3766634077
Szymon Lesisz <sz.lesisz@gmail.com>
2018-11-06 12:35:09 +0100

    popup: FW update redirection to proper target

--- src/js/popup/view/notification.js
+++ src/js/popup/view/notification.js
@@ -2,19 +2,29 @@
 'use strict';
 
 import { views } from './common';
+import { getLatestRelease } from '../../data/FirmwareInfo';
+import type { UnexpectedDeviceMode } from '../../types/ui-request';
 
-export const showFirmwareUpdateNotification = (): void => {
+export const showFirmwareUpdateNotification = (device: $PropertyType<UnexpectedDeviceMode, 'payload'>): void => {
     const container: HTMLElement = document.getElementsByClassName('notification')[0];
     const warning: ?HTMLElement = container.querySelector('.firmware-update-notification');
     if (warning) {
         // already exists
         return;
     }
+    if (!device.features) return;
+    const { features } = device;
+    const release = getLatestRelease([ features.major_version, features.minor_version, features.patch_version ]);
 
     const view = views.getElementsByClassName('firmware-update-notification');
     const notification = document.createElement('div');
     notification.className = 'firmware-update-notification notification-item';
     notification.innerHTML = view.item(0).innerHTML;
+
+    if (release && release.beta) {
+        const button = notification.getElementsByClassName('notification-button')[0];
+        button.setAttribute('href', 'https://beta-wallet.trezor.io/');
+    }
 
     container.appendChild(notification);
 
