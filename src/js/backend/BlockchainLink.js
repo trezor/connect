a324021847fd39a402b97174b86ce825b315b0cd
Szymon Lesisz <sz.lesisz@gmail.com>
2018-11-29 17:52:09 +0100

    BlockchainLink class: flowtype + eslint fixes + add "init" method

--- src/js/backend/BlockchainLink.js
+++ src/js/backend/BlockchainLink.js
@@ -4,57 +4,23 @@ import BlockchainLink from 'blockchain-link';
 /* $FlowIssue loader notation */
 import RippleWorker from 'file-loader?name=ripple4-worker.js!blockchain-link/workers/ripple-worker';
 
-import type { CoinInfo, EthereumNetworkInfo, MiscNetworkInfo } from 'flowtype';
-
-type CombinedCoinInfo = CoinInfo | EthereumNetworkInfo | MiscNetworkInfo;
-
-const instances: Array<Blockchain> = [];
-
-const find = (name: string): ?Blockchain => {
-    for (let i: number = 0; i < instances.length; i++) {
-        if (instances[i].coinInfo.name === name) {
-            if (instances[i].error) {
-                remove(instances[i]);
-            } else {
-                return instances[i];
-            }
-        }
-    }
-    return null;
-};
-
-export const create = async (coinInfo: CombinedCoinInfo): Promise<Blockchain> => {
-    let backend: ?Blockchain = find(coinInfo.name);
-    if (!backend) {
-        backend = new Blockchain({coinInfo});
-        // try {
-        //     await backend.loadCoinInfo(coinInfo);
-        // } catch (error) {
-        //     remove(backend);
-        //     throw error;
-        // }
-        instances.push(backend);
-    }
-    return backend;
-};
-
-const remove = (backend: Blockchain): void => {
-    const index: number = instances.indexOf(backend);
-    if (index >= 0) {
-        instances.splice(index, 1);
-    }
-};
+import type { CoinInfo } from '../types';
+import { BlockchainMessage } from '../message/builder';
+import * as BLOCKCHAIN from '../constants/blockchain';
 
 type Options = {
-    coinInfo: CombinedCoinInfo,
+    coinInfo: CoinInfo,
+    postMessage: (message: BlockchainMessage) => void,
 };
 
 export default class Blockchain {
-    coinInfo: CombinedCoinInfo;
+    coinInfo: $ElementType<Options, 'coinInfo'>;
+    postMessage: $ElementType<Options, 'postMessage'>;
     error: boolean;
 
     constructor(options: Options) {
         this.coinInfo = options.coinInfo;
+        this.postMessage = options.postMessage;
 
         BlockchainLink.create({
             name: this.coinInfo.shortcut,
@@ -64,6 +30,32 @@ export default class Blockchain {
             ],
             debug: true,
         });
+    }
+
+    async init() {
+        const blockchain = BlockchainLink.get(this.coinInfo.shortcut);
+        blockchain.on('error', error => {
+            this.postMessage(new BlockchainMessage(BLOCKCHAIN.ERROR, {
+                coin: this.coinInfo,
+                error: error.message,
+            }));
+        });
+
+        const networkInfo = await blockchain.getInfo();
+        const fee = await blockchain.getFee();
+
+        this.postMessage(new BlockchainMessage(BLOCKCHAIN.CONNECT, {
+            coin: this.coinInfo,
+            info: {
+                fee,
+                ...networkInfo,
+            },
+        }));
+    }
+
+    async getNetworkInfo() {
+        const blockchain = BlockchainLink.get(this.coinInfo.shortcut);
+        return await blockchain.getInfo();
     }
 
     async getAccountInfo(descriptor: string) {
@@ -78,23 +70,30 @@ export default class Blockchain {
         return await blockchain.getFee();
     }
 
-    async subscribe(accounts: Array<string>,
-        blockHandler: (hash: string, height: number) => void,
-        notificationHandler: (notification: any) => void,
-        errorHandler: (error: Error) => void,
-    ): Promise<void> {
+    async subscribe(accounts: Array<string>): Promise<void> {
         const blockchain = BlockchainLink.get(this.coinInfo.shortcut);
 
-        blockchain.on('block', blockHandler);
-        blockchain.on('address', notificationHandler);
-        blockchain.on('error', errorHandler);
+        blockchain.on('block', (hash, block) => {
+            this.postMessage(new BlockchainMessage(BLOCKCHAIN.BLOCK, {
+                coin: this.coinInfo,
+                hash,
+                block,
+            }));
+        });
+
+        blockchain.on('notification', notification => {
+            this.postMessage(new BlockchainMessage(BLOCKCHAIN.NOTIFICATION, {
+                coin: this.coinInfo,
+                notification,
+            }));
+        });
 
         blockchain.subscribe({
             type: 'block',
         });
 
         blockchain.subscribe({
-            type: 'address',
+            type: 'notification',
             addresses: accounts,
         });
     }
@@ -104,3 +103,43 @@ export default class Blockchain {
         return await blockchain.pushTransaction(tx);
     }
 }
+
+const instances: Array<Blockchain> = [];
+
+const remove = (backend: Blockchain): void => {
+    const index: number = instances.indexOf(backend);
+    if (index >= 0) {
+        instances.splice(index, 1);
+    }
+};
+
+const find = (name: string): ?Blockchain => {
+    for (let i: number = 0; i < instances.length; i++) {
+        if (instances[i].coinInfo.name === name) {
+            if (instances[i].error) {
+                remove(instances[i]);
+            } else {
+                return instances[i];
+            }
+        }
+    }
+    return null;
+};
+
+export const create = async (coinInfo: $ElementType<Options, 'coinInfo'>, postMessage: $ElementType<Options, 'postMessage'>): Promise<Blockchain> => {
+    let backend: ?Blockchain = find(coinInfo.name);
+    if (!backend) {
+        backend = new Blockchain({
+            coinInfo,
+            postMessage,
+        });
+        try {
+            await backend.init();
+        } catch (error) {
+            remove(backend);
+            throw error;
+        }
+        instances.push(backend);
+    }
+    return backend;
+};
