1eecb57cb0b8fc4587e3c771da3243e3b502ee65
Pavol Rusnak <pavol@rusnak.io>
2019-01-07 18:07:27 +0100

    new version 6.0.3

--- src/js/data/ConnectSettings.js
+++ src/js/data/ConnectSettings.js
@@ -27,7 +27,7 @@ export type ConnectSettings = {
  * It could be changed by passing values into TrezorConnect.init(...) method
  */
 
-const VERSION: string = '6.0.2';
+const VERSION: string = '6.0.3';
 const versionN: Array<number> = VERSION.split('.').map(s => parseInt(s));
 const DIRECTORY: string = `${ versionN[0] }${ (versionN[1] > 0 ? `.${versionN[1]}` : '') }/`;
 const DEFAULT_DOMAIN: string = `https://connect.trezor.io/${ DIRECTORY }`;
