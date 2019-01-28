36d280c722d7f0ac492090d1662373c6ff453cdb
Szymon Lesisz <sz.lesisz@gmail.com>
2019-01-28 16:16:37 +0100

    change GetAddress method logic

--- src/js/core/methods/GetAddress.js
+++ src/js/core/methods/GetAddress.js
@@ -3,7 +3,7 @@
 
 import AbstractMethod from './AbstractMethod';
 import { validateParams, validateCoinPath, getRequiredFirmware } from './helpers/paramsValidator';
-import { validatePath, getLabel } from '../../utils/pathUtils';
+import { validatePath, getLabel, getSerializedPath } from '../../utils/pathUtils';
 import { getCoinInfoByCurrency, getCoinInfoFromPath, fixCoinInfoNetwork } from '../../data/CoinInfo';
 import { NO_COIN_INFO } from '../../constants/errors';
 import { uniqBy } from 'lodash';
@@ -12,20 +12,20 @@ import * as UI from '../../constants/ui';
 import { UiMessage } from '../../message/builder';
 
 import type { Address } from '../../types/trezor';
-import type { CoinInfo } from 'flowtype';
+import type { CoinInfo, UiPromiseResponse } from 'flowtype';
 import type { CoreMessage } from '../../types';
 
 type Batch = {
     path: Array<number>,
+    address: ?string,
     coinInfo: CoinInfo,
     showOnTrezor: boolean,
 }
-type Params = {
-    bundle: Array<Batch>,
-    bundledResponse: boolean,
-}
+
+type Params = Array<Batch>;
 
 export default class GetAddress extends AbstractMethod {
+    confirmed: boolean = false;
     params: Params;
 
     constructor(message: CoreMessage) {
@@ -33,26 +33,22 @@ export default class GetAddress extends AbstractMethod {
 
         this.requiredPermissions = ['read'];
 
-        const payload: Object = message.payload;
-        let bundledResponse: boolean = true;
         // create a bundle with only one batch
-        if (!payload.hasOwnProperty('bundle')) {
-            payload.bundle = [ ...payload ];
-            bundledResponse = false;
-        }
+        const payload: Object = !message.payload.hasOwnProperty('bundle') ? { ...message.payload, bundle: [ ...message.payload ] } : message.payload;
 
         // validate bundle type
         validateParams(payload, [
             { name: 'bundle', type: 'array' },
+            { name: 'useEventListener', type: 'boolean' },
         ]);
 
         const bundle = [];
-        let shouldUseUi: boolean = false;
         payload.bundle.forEach(batch => {
             // validate incoming parameters for each batch
             validateParams(batch, [
                 { name: 'path', obligatory: true },
                 { name: 'coin', type: 'string' },
+                { name: 'address', type: 'string' },
                 { name: 'showOnTrezor', type: 'boolean' },
             ]);
 
@@ -72,9 +68,6 @@ export default class GetAddress extends AbstractMethod {
             if (batch.hasOwnProperty('showOnTrezor')) {
                 showOnTrezor = batch.showOnTrezor;
             }
-            if (showOnTrezor) {
-                shouldUseUi = true;
-            }
 
             if (!coinInfo) {
                 throw NO_COIN_INFO;
@@ -88,13 +81,15 @@ export default class GetAddress extends AbstractMethod {
 
             bundle.push({
                 path,
+                address: batch.address,
                 coinInfo,
                 showOnTrezor,
             });
         });
 
-        // this.useUi = !(!shouldUseUi && bundle.length < 2);
-        this.useUi = shouldUseUi;
+        const useEventListener = payload.useEventListener && payload.bundle.length === 1 && typeof payload.bundle[0].address === 'string' && payload.bundle[0].showOnTrezor;
+        this.confirmed = useEventListener;
+        this.useUi = !useEventListener;
 
         // set info
         if (bundle.length === 1) {
@@ -109,23 +104,76 @@ export default class GetAddress extends AbstractMethod {
             }
         }
 
-        this.params = {
-            bundle,
-            bundledResponse,
-        };
+        this.params = bundle;
+    }
+
+    getButtonRequestData(code: string) {
+        if (code === 'ButtonRequest_Address' && this.params.length === 1) {
+            const data = [];
+            for (let i = 0; i < this.params.length; i++) {
+                data.push({
+                    serializedPath: getSerializedPath(this.params[i].path),
+                    address: this.params[i].address || 'not-set',
+                });
+            }
+            return data;
+        }
+        return null;
+    }
+
+    async confirmation(): Promise<boolean> {
+        if (this.confirmed) return true;
+        // wait for popup window
+        await this.getPopupPromise().promise;
+        // initialize user response promise
+        const uiPromise = this.createUiPromise(UI.RECEIVE_CONFIRMATION, this.device);
+
+        const label: string = this.info;
+        // request confirmation view
+        this.postMessage(new UiMessage(UI.REQUEST_CONFIRMATION, {
+            view: 'export-address',
+            label,
+        }));
+
+        // wait for user action
+        const uiResp: UiPromiseResponse = await uiPromise.promise;
+        const resp: string = uiResp.payload;
+
+        this.confirmed = (resp === 'true');
+        return this.confirmed;
     }
 
     async run(): Promise<Address | Array<Address>> {
         const responses: Array<Address> = [];
-        for (let i = 0; i < this.params.bundle.length; i++) {
+        const bundledResponse = this.params.length > 1;
+
+        if (!bundledResponse) {
+            // silently get address and compare with requested address
+            // or display as default inside popup
+            const item = this.params[0];
+            const response: Address = await this.device.getCommands().getAddress(
+                item.path,
+                item.coinInfo,
+                false
+            );
+            if (typeof item.address === 'string') {
+                if (item.address !== response.address) {
+                    throw new Error('Addresses do not match');
+                }
+            } else {
+                this.params[0].address = response.address;
+            }
+        }
+
+        for (let i = 0; i < this.params.length; i++) {
             const response:Address = await this.device.getCommands().getAddress(
-                this.params.bundle[i].path,
-                this.params.bundle[i].coinInfo,
-                this.params.bundle[i].showOnTrezor
+                this.params[i].path,
+                this.params[i].coinInfo,
+                this.params[i].showOnTrezor
             );
             responses.push(response);
 
-            if (this.params.bundledResponse) {
+            if (bundledResponse) {
                 // send progress
                 this.postMessage(new UiMessage(UI.BUNDLE_PROGRESS, {
                     progress: i,
@@ -133,6 +181,6 @@ export default class GetAddress extends AbstractMethod {
                 }));
             }
         }
-        return this.params.bundledResponse ? responses : responses[0];
+        return bundledResponse ? responses : responses[0];
     }
 }
