de8eca58b1b4d26bd3420cfad8075d311524c4be
Szymon Lesisz <sz.lesisz@gmail.com>
2019-02-05 14:03:04 +0100

    Update run.sh

--- src/__tests__/run.sh
+++ src/__tests__/run.sh
@@ -239,7 +239,8 @@ start_emulator() {
 start_transport() {
     cd $trezord_path
     #./trezord-go -e 21324 > /dev/null 2>&1 &
-    ./trezord-go -e 21324 -e 21325 > /dev/null 2>&1 &
+    #./trezord-go -e 21324 -e 21325 > /dev/null 2>&1 &
+    ./trezord-go -ed 21324:21325 > /dev/null 2>&1 &
     #./trezord-go -e 21324 -e 21325 &
     #./trezord-go -e 21325 > /dev/null 2>&1 &
     #./trezord-go &
