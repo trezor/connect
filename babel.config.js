0432afc22677a60a9132c19c9f84ee91edbc39ca
Szymon Lesisz <sz.lesisz@gmail.com>
2019-02-18 17:48:25 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,7 @@ module.exports = (api) => {
     ];
 
     const plugins = [
+        '@babel/plugin-transform-flow-strip-types',
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-object-rest-spread',
         ['@babel/plugin-transform-runtime', {
