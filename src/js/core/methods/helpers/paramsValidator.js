58071ce8c56cfeb9eb45a6e7270e3f1164bd4b95
Szymon Lesisz <sz.lesisz@gmail.com>
2019-04-09 16:05:01 +0200

    allow SignTx:refTx to be empty array

--- src/js/core/methods/helpers/paramsValidator.js
+++ src/js/core/methods/helpers/paramsValidator.js
@@ -10,8 +10,9 @@ import type { CoinInfo, FirmwareRange } from '../../../types';
 
 type Param = {
     name: string,
-    type?: 'string' | 'number' | 'array' | 'buffer' | 'boolean' | 'amount' | 'object',
-    obligatory?: true,
+    type?: string,
+    obligatory?: boolean,
+    allowEmpty?: true,
 }
 
 export const validateParams = (values: Object, fields: Array<Param>): void => {
@@ -23,7 +24,7 @@ export const validateParams = (values: Object, fields: Array<Param>): void => {
                     if (!Array.isArray(value)) {
                         // invalid type
                         throw invalidParameter(`Parameter "${ field.name }" has invalid type. "${ field.type }" expected.`);
-                    } else if (value.length < 1) {
+                    } else if (!field.allowEmpty && value.length < 1) {
                         throw invalidParameter(`Parameter "${ field.name }" is empty.`);
                     }
                 } else if (field.type === 'amount') {
