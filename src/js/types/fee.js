e23b442f5590a59a9157e1746732cdb8461b8e5e
Szymon Lesisz <sz.lesisz@gmail.com>
2019-07-17 19:01:45 +0200

    remove old BlockBook fees logic

--- src/js/types/fee.js
+++ src/js/types/fee.js
@@ -1,61 +1,38 @@
 /* @flow */
 
-export type BlockFees = {[i: number]: string}; // blocks => sat/B
-
-type LegacyBitcoreFeeLevelInfo = {
-    +type: 'bitcore-legacy',
-    +blocks: number,
-};
-
-export type LegacyBitcoreFeeLevel = {
-    +name: string,
-    +id: number,
-    +info: LegacyBitcoreFeeLevelInfo,
-}
-
-type SmartBitcoreFeeLevelInfo = {
-    +type: 'bitcore-smart',
-    +blocks: number,
-};
-
-export type SmartBitcoreFeeLevel = {
-    +name: string,
-    +id: number,
-    +info: SmartBitcoreFeeLevelInfo,
-}
-
-type PreloadedFeeLevelInfo = {
-    +type: 'preloaded',
-    +fee: string,
-}
-
-type CustomFeeLevelInfo = {
-    +type: 'custom',
-    fee: string,
-}
-
-export type CustomFeeLevel = {
-    +name: string,
-    +id: number,
-    +info: CustomFeeLevelInfo,
-}
-
-export type FeeLevelInfo = LegacyBitcoreFeeLevelInfo | SmartBitcoreFeeLevelInfo | PreloadedFeeLevelInfo | CustomFeeLevelInfo;
-
 export type SelectFeeLevel = {
-    +name: string,
-    +fee: '0',
-    +disabled: true,
+    name: string,
+    fee: '0',
+    disabled: true,
 } | {
-    +name: string,
-    +fee: string,
-    +feePerByte: string,
-    +minutes: number,
-    +total: string,
+    name: string,
+    fee: string,
+    feePerByte: string,
+    minutes: number,
+    total: string,
 }
 
 export type FeeLevel = {
-    +name: string,
-    +id: number,
-    +info: FeeLevelInfo,
+    type: 'bitcoin',
+    name: 'high' | 'normal' | 'economy' | 'low' | 'custom',
+    info: {
+        fee: string,
+        blocks: number,
+    },
+} | {
+    type: 'ethereum',
+    name: string,
+    info: {
+        fee: string,
+        gasLimit: string,
+        gasPrice: string,
+        blocks: number,
+    },
+} | {
+    type: 'ripple',
+    name: string,
+    info: {
+        fee: string,
+        blocks: number,
+    },
 }
