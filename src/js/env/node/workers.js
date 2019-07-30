ffa18b3fa775d11ed91e4bb47c640a5c8a8d07c1
Szymon Lesisz <sz.lesisz@gmail.com>
2019-07-30 18:59:47 +0200

    update env/workers (remove artifacts from hd-wallet)

--- src/js/env/node/workers.js
+++ src/js/env/node/workers.js
@@ -1,32 +1,17 @@
-import path from 'path';
 import TinyWorker from 'tiny-worker';
 
 export const SharedConnectionWorker = () => {
     return null;
 };
 
-export const FastXpubWasm = path.resolve(process.cwd(), './node_modules/hd-wallet/lib/fastxpub/fastxpub.wasm');;
-
-export const FastXpubWorker = () => {
+export const BlockbookWorker = () => {
     return new TinyWorker(() => {
-        require('hd-wallet/lib/fastxpub/fastxpub');
-    });
-};
-
-export const DiscoveryWorker = () => {
-    return new TinyWorker(() => {
-        require('hd-wallet/lib/discovery/worker/inside');
-    });
-};
-
-export const SocketWorker = () => {
-    return new TinyWorker(() => {
-        require('hd-wallet/lib/socketio-worker/inside');
+        require('@trezor/blockchain-link/build/node/blockbook-worker');
     });
 };
 
 export const RippleWorker = () => {
     return new TinyWorker(() => {
-        require('trezor-blockchain-link/lib/workers/ripple/index');
+        require('@trezor/blockchain-link/build/node/ripple-worker');
     });
 };
