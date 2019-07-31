72a45b372c282c8191632fbd041cfd4f10e81f56
Szymon Lesisz <sz.lesisz@gmail.com>
2019-07-31 18:11:22 +0200

    fix blockbook-worker name

--- src/js/env/browser/workers.js
+++ src/js/env/browser/workers.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import SharedConnectionWorker from 'sharedworker-loader?name=js/shared-connection-worker.[hash].js!trezor-link/lib/lowlevel/sharedConnectionWorker';
-import BlockbookWorker from 'worker-loader?name=workers/ripple-worker.[hash].js!@trezor/blockchain-link/lib/workers/blockbook/index.js';
+import BlockbookWorker from 'worker-loader?name=workers/blockbook-worker.[hash].js!@trezor/blockchain-link/lib/workers/blockbook/index.js';
 import RippleWorker from 'worker-loader?name=workers/ripple-worker.[hash].js!@trezor/blockchain-link/lib/workers/ripple/index.js';
 
 export {
