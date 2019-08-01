10ed1aa5ad8188ccb3276791790553dd1f274cc6
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-01 11:49:01 +0200

    build SharedConnectionWorker to build/workers dir

--- src/js/env/browser/workers.js
+++ src/js/env/browser/workers.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import SharedConnectionWorker from 'sharedworker-loader?name=js/shared-connection-worker.[hash].js!trezor-link/lib/lowlevel/sharedConnectionWorker';
+import SharedConnectionWorker from 'sharedworker-loader?name=workers/shared-connection-worker.[hash].js!trezor-link/lib/lowlevel/sharedConnectionWorker';
 import BlockbookWorker from 'worker-loader?name=workers/blockbook-worker.[hash].js!@trezor/blockchain-link/lib/workers/blockbook/index.js';
 import RippleWorker from 'worker-loader?name=workers/ripple-worker.[hash].js!@trezor/blockchain-link/lib/workers/ripple/index.js';
 
