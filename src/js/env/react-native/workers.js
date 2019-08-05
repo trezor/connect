d5bc839cdfff6efb7ba692e0d800b61ff62b93a1
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-05 13:07:26 +0200

    fix react-native workers

--- src/js/env/react-native/workers.js
+++ src/js/env/react-native/workers.js
@@ -1,24 +1,14 @@
-import { Thread } from 'react-native-threads';
-
-// webpack needs to compile and export those files
-/* eslint-disable no-unused-vars */
-// import RippleWorkerLoader from 'worker-loader?name=js/ripple-worker.js!trezor-blockchain-link/lib/workers/ripple/index.js';
-// import RippleWorkerLoader from 'worker-loader?name=js/ripple-worker.js!react-native-worker!trezor-blockchain-link/lib/workers/ripple/index.js';
-/* eslint-enable no-unused-vars */
-
-// import DiscoveryWorkerL from 'react-native-worker!hd-wallet/lib/discovery/worker/inside';
-// import SocketWorkerL from 'react-native-worker?name=js/socketio-worker.js!hd-wallet/lib/socketio-worker/inside';
-// import RippleWorkerL from 'worker-loader?name=js/ripple-worker.js!react-native-worker!trezor-blockchain-link/lib/workers/ripple/index.js';
+import BlockbookWorkerWrapper from '@trezor/blockchain-link/build/module/blockbook-worker.js';
+import RippleWorkerWrapper from '@trezor/blockchain-link/build/module/ripple-worker.js';
 
 export const SharedConnectionWorker = () => {
     return null;
 };
 
 export const BlockbookWorker = () => {
-    return new Thread('./packages/native/trezor-connect/js/ripple-worker.js');
+    return new BlockbookWorkerWrapper();
 };
 
 export const RippleWorker = () => {
-    // return new RippleWorkerL();
-    return new Thread('./packages/native/trezor-connect/js/ripple-worker.js');
+    return new RippleWorkerWrapper();
 };
