ba12061913dda2d14154f85fce31fe866389d006
Szymon Lesisz <sz.lesisz@gmail.com>
2019-09-30 14:04:13 +0200

    fix unnecesarry wrapped error

--- src/js/core/Core.js
+++ src/js/core/Core.js
@@ -581,7 +581,7 @@ export const onCall = async (message: CoreMessage): Promise<void> => {
                 const response: Object = await method.run();
                 messageResponse = new ResponseMessage(method.responseID, true, response);
             } catch (error) {
-                return Promise.reject({ error: error.message, code: error.code });
+                return Promise.reject(error);
             }
         };
 
