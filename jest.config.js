cf4e4a4ba0cbccdaa3ba05a03c12ed4433432b62
Szymon Lesisz <sz.lesisz@gmail.com>
2019-11-22 17:56:07 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,10 @@ module.exports = {
     testMatch: ['**/tests/device/**/*.test.(js)'],
     // coverageDirectory: './coverage/',
     // collectCoverage: true,
-    // collectCoverageFrom: ['**/src/**/*.js'],
-    // modulePathIgnorePatterns: ['node_modules'],
-    setupFiles: ['./tests/common.setup.js', './tests/jest.setup.js'],
+    // collectCoverageFrom: ['**/src/js/**/*.js'],
+    modulePathIgnorePatterns: ['node_modules', '_old', 'src/types', 'src/ui', 'src/utils/ws.ts'],
+    // setupFiles: ['.', './tests/jest.setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js', '<rootDir>/tests/common.setup.js'],
     globalSetup: './tests/jest.globalSetup.js',
     globalTeardown: './tests/jest.globalTeardown.js',
     transform: {
