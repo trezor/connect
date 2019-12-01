3dbde8ff00bec37506275d44c670eb8ff758c4b6
Szymon Lesisz <sz.lesisz@gmail.com>
2019-12-01 16:39:51 +0100

    DeviceList initialization refactor

--- src/js/device/DeviceList.js
+++ src/js/device/DeviceList.js
@@ -11,31 +11,28 @@ import type { Device as DeviceTyped } from '../types';
 import TrezorLink from 'trezor-link';
 import type { Transport, TrezorDeviceInfoWithSession as DeviceDescriptor } from 'trezor-link';
 import DataManager from '../data/DataManager';
+import { getBridgeInfo } from '../data/TransportInfo';
 import Log, { init as initLog } from '../utils/debug';
 import { resolveAfter } from '../utils/promiseUtils';
 
 import { WebUsbPlugin, ReactNativeUsbPlugin } from '../env/node/workers';
 const { BridgeV2, Fallback } = TrezorLink;
 
-export type DeviceListOptions = {
-    debug?: boolean,
-    debugInfo?: boolean,
-    transport?: Transport,
-    nodeTransport?: Transport,
-    configUrl?: string,
-    config?: string,
-    bridgeVersionUrl?: string,
-    clearSession?: boolean,
-    clearSessionTime?: number,
-    rememberDevicePassphrase?: boolean,
-};
-
 // custom log
 const _log: Log = initLog('DeviceList');
 
+// TODO: plugins are not typed in 'trezor-link'
+type LowLevelPlugin = {
+    name: 'WebUsbPlugin' | 'ReactNativePlugin',
+    unreadableHidDeviceChange: {
+        on: (event: string, fn: any) => void,
+    },
+    unreadableHidDevice: boolean, // not sure
+}
+
 export default class DeviceList extends EventEmitter {
-    options: DeviceListOptions;
     transport: Transport;
+    transportPlugin: LowLevelPlugin | typeof undefined;
     stream: DescriptorStream;
     devices: {[k: string]: Device} = {};
     creatingDevicesDescriptors: {[k: string]: DeviceDescriptor} = {};
@@ -45,57 +42,50 @@ export default class DeviceList extends EventEmitter {
     hasCustomMessages: boolean = false;
     transportStartPending: number = 0;
 
-    constructor(options: ?DeviceListOptions) {
+    constructor() {
         super();
-        this.options = options || {};
 
-        _log.enabled = DataManager.getSettings('debug');
-        if (!this.options.transport) {
+        const { debug, env, webusb } = DataManager.settings;
+        _log.enabled = debug;
+
+        const transports: Transport[] = [];
+
+        if (env === 'react-native' && typeof ReactNativeUsbPlugin !== 'undefined') {
+            transports.push(ReactNativeUsbPlugin());
+        } else {
             // $FlowIssue: `version` is missing in `JSON`
-            const bridgeVersion = DataManager.assets['bridge'].version.join('.');
-            const env = DataManager.getSettings('env');
+            const bridgeVersion: string = getBridgeInfo().version.join('.');
             if (env === 'node' || env === 'electron') {
                 BridgeV2.setFetch(fetch, true);
             }
-
-            const transportTypes: Transport[] = [
-                new BridgeV2(null, null, bridgeVersion),
-            ];
-
-            if (env === 'react-native' && typeof ReactNativeUsbPlugin !== 'undefined') {
-                transportTypes.splice(0, 1); // remove bridge
-                transportTypes.push(ReactNativeUsbPlugin());
-            }
-
-            if (DataManager.getSettings('webusb') && typeof WebUsbPlugin !== 'undefined') {
-                transportTypes.push(WebUsbPlugin());
-            }
-
-            this.options.transport = new Fallback(transportTypes);
+            transports.push(new BridgeV2(null, null, bridgeVersion));
         }
-        if (this.options.debug === undefined) {
-            this.options.debug = true; // DataManager.getDebugSettings('deviceList');
+
+        if (webusb && typeof WebUsbPlugin !== 'undefined') {
+            transports.push(WebUsbPlugin());
         }
+
+        this.transport = new Fallback(transports);
+        this.defaultMessages = DataManager.getMessages();
+        this.currentMessages = this.defaultMessages;
     }
 
-    async init(): Promise<void> {
+    async init() {
+        const { transport } = this;
         try {
-            this.transport = await this._initTransport();
-            await this._initStream();
-            const webUsbPlugin = this.getWebUsbPlugin();
-            if (webUsbPlugin) {
-                webUsbPlugin.unreadableHidDeviceChange.on('change', async () => {
-                    if (webUsbPlugin.unreadableHidDevice) {
-                        const device = await this._createUnacquiredDevice({ path: DEVICE.UNREADABLE, session: null, debugSession: null, debug: false });
-                        this.devices[DEVICE.UNREADABLE] = device;
-                        this.emit(DEVICE.CONNECT_UNACQUIRED, device.toMessageObject());
-                    } else {
-                        const device = this.devices[DEVICE.UNREADABLE];
-                        delete this.devices[DEVICE.UNREADABLE];
-                        this.emit(DEVICE.DISCONNECT, device.toMessageObject());
-                    }
-                });
+            _log.debug('Initializing transports');
+            await transport.init(_log.enabled);
+            _log.debug('Configuring transports');
+            await transport.configure(this.defaultMessages);
+            _log.debug('Configuring transports done');
+
+            const { activeName } = transport;
+            if (activeName === 'LowlevelTransportWithSharedConnections') {
+                // $FlowIssue "activeTransport" is not typed in trezor-link
+                this.transportPlugin = transport.activeTransport.plugin;
             }
+
+            await this._initStream();
 
             // listen for self emitted events and resolve pending transport event if needed
             this.on(DEVICE.CONNECT, this.resolveTransportEvent.bind(this));
@@ -105,29 +95,7 @@ export default class DeviceList extends EventEmitter {
         }
     }
 
-    async _initTransport(): Promise<Transport> {
-        const transport = this.options.transport;
-        if (!transport) throw ERROR.NO_TRANSPORT;
-        _log.debug('Initializing transports');
-        await transport.init(DataManager.getSettings('debug'));
-        // await transport.init(false);
-        _log.debug('Configuring transports');
-        await this._configTransport(transport);
-        _log.debug('Configuring transports done');
-        return transport;
-    }
-
-    async _configTransport(transport: Transport): Promise<void> {
-        try {
-            this.defaultMessages = DataManager.getMessages();
-            this.currentMessages = this.defaultMessages;
-            await transport.configure(this.defaultMessages);
-        } catch (error) {
-            throw ERROR.WRONG_TRANSPORT_CONFIG;
-        }
-    }
-
-    async reconfigure(json: JSON, custom?: boolean): Promise<void> {
+    async reconfigure(json: JSON, custom?: boolean) {
         if (this.currentMessages === json) return;
         try {
             await this.transport.configure(json);
@@ -138,7 +106,7 @@ export default class DeviceList extends EventEmitter {
         }
     }
 
-    async restoreMessages(): Promise<void> {
+    async restoreMessages() {
         if (!this.hasCustomMessages) return;
         try {
             await this.transport.configure(this.defaultMessages);
@@ -148,16 +116,14 @@ export default class DeviceList extends EventEmitter {
         }
     }
 
-    resolveTransportEvent(): void {
-        if (this.transportStartPending > 0) {
-            this.transportStartPending--;
-            if (this.transportStartPending === 0) {
-                this.stream.emit(TRANSPORT.START);
-            }
+    resolveTransportEvent() {
+        this.transportStartPending--;
+        if (this.transportStartPending === 0) {
+            this.stream.emit(TRANSPORT.START);
         }
     }
 
-    async waitForTransportFirstEvent(): Promise<void> {
+    async waitForTransportFirstEvent() {
         await new Promise(resolve => {
             const handler = () => {
                 this.removeListener(TRANSPORT.START, handler);
@@ -174,23 +140,18 @@ export default class DeviceList extends EventEmitter {
      * @param {Transport} transport
      * @memberof DeviceList
      */
-    async _initStream(): Promise<void> {
-        const stream: DescriptorStream = new DescriptorStream(this.transport);
+    async _initStream() {
+        const stream = new DescriptorStream(this.transport);
 
-        stream.on(TRANSPORT.START_PENDING, (pending: number): void => {
+        stream.on(TRANSPORT.START_PENDING, (pending: number) => {
             this.transportStartPending = pending;
         });
 
-        stream.on(TRANSPORT.START, (): void => {
-            this.emit(TRANSPORT.START, {
-                type: this.transportType(),
-                version: this.transportVersion(),
-                outdated: this.transportOutdated(),
-                bridge: DataManager.getLatestBridgeVersion(),
-            });
+        stream.on(TRANSPORT.START, () => {
+            this.emit(TRANSPORT.START, this._getTransportInfo());
         });
 
-        stream.on(TRANSPORT.UPDATE, (diff: DeviceDescriptorDiff): void => {
+        stream.on(TRANSPORT.UPDATE, (diff: DeviceDescriptorDiff) => {
             new DiffHandler(this, diff).handle();
         });
 
@@ -202,19 +163,30 @@ export default class DeviceList extends EventEmitter {
         stream.listen();
         this.stream = stream;
 
+        if (this.transportPlugin && this.transportPlugin.name === 'WebUsbPlugin') {
+            const { unreadableHidDevice, unreadableHidDeviceChange } = this.transportPlugin;
+            unreadableHidDeviceChange.on('change', async () => {
+                if (unreadableHidDevice) {
+                    const device = await this._createUnacquiredDevice({ path: DEVICE.UNREADABLE, session: null, debugSession: null, debug: false });
+                    this.devices[DEVICE.UNREADABLE] = device;
+                    this.emit(DEVICE.CONNECT_UNACQUIRED, device.toMessageObject());
+                } else {
+                    const device = this.devices[DEVICE.UNREADABLE];
+                    delete this.devices[DEVICE.UNREADABLE];
+                    this.emit(DEVICE.DISCONNECT, device.toMessageObject());
+                }
+            });
+        }
+
         this.emit(TRANSPORT.STREAM, stream);
     }
 
-    async _createAndSaveDevice(
-        descriptor: DeviceDescriptor
-    ): Promise<void> {
+    async _createAndSaveDevice(descriptor: DeviceDescriptor) {
         _log.debug('Creating Device', descriptor);
         await new CreateDeviceHandler(descriptor, this).handle();
     }
 
-    async _createUnacquiredDevice(
-        descriptor: DeviceDescriptor
-    ): Promise<Device> {
+    async _createUnacquiredDevice(descriptor: DeviceDescriptor) {
         const currentDescriptor = (this.stream.current && this.stream.current.find(d => d.path === descriptor.path)) || descriptor;
         _log.debug('Creating Unacquired Device', currentDescriptor);
 
@@ -225,88 +197,48 @@ export default class DeviceList extends EventEmitter {
         return device;
     }
 
-    getDevice(path: string): Device {
+    getDevice(path: string) {
         return this.devices[path];
     }
 
-    getFirstDevicePath(): string {
-        // const first = this.asArray()[0];
-        // return this.devices[first.path];
-        // const arr: Array<Object> =
+    getFirstDevicePath() {
         return this.asArray()[0].path;
     }
 
-    asArray(): Array<DeviceTyped> {
-        const list: Array<DeviceTyped> = this.allDevices().map(device => device.toMessageObject());
-        return list;
+    asArray(): DeviceTyped[] {
+        return this.allDevices().map(device => device.toMessageObject());
     }
 
-    allDevices(): Array<Device> {
+    allDevices(): Device[] {
         return Object.keys(this.devices).map((key: string) => this.devices[key]);
     }
 
-    length(): number {
+    length() {
         return this.asArray().length;
     }
 
-    transportType(): string {
-        if (this.transport == null) {
-            return '';
+    transportType() {
+        const { transport, transportPlugin } = this;
+        const { activeName } = transport;
+        if (activeName === 'BridgeTransport') {
+            return 'bridge';
         }
-        const activeName: ?string = this.transport.activeName;
-        if (activeName) {
-            if (activeName === 'BridgeTransport') {
-                return 'bridge';
-            }
-            if (activeName === 'LowlevelTransportWithSharedConnections') {
-                return 'webusb';
-            }
-            return activeName;
+        if (transportPlugin) {
+            return transportPlugin.name;
         }
-        return this.transport.name;
+        return transport.name;
     }
 
-    transportVersion(): string {
-        if (this.transport == null) {
-            return '';
-        }
-        return this.transport.version;
+    _getTransportInfo() {
+        return {
+            type: this.transportType(),
+            version: this.transport.version,
+            outdated: this.transport.isOutdated,
+        };
     }
 
-    transportOutdated(): boolean {
-        if (this.transport == null) {
-            return false;
-        }
-        if (this.transport.isOutdated) {
-            return true;
-        }
-        return false;
-    }
-
-    getWebUsbPlugin(): any {
-        try {
-            const transport: ?Transport = this.transport;
-            if (!transport) {
-                return null;
-            }
-
-            // $FlowIssue - this all is going around Flow :/
-            const activeTransport = transport.activeTransport;
-            if (!activeTransport || activeTransport.name !== 'LowlevelTransportWithSharedConnections') {
-                return null;
-            }
-            const webusbTransport = activeTransport.plugin;
-            if (!webusbTransport || webusbTransport.name !== 'WebUsbPlugin') {
-                return null;
-            }
-            return webusbTransport;
-        } catch (e) {
-            return null;
-        }
-    }
-
-    onBeforeUnload(clearSession?: ?boolean) {
-        if (this.stream !== null) {
+    onBeforeUnload() {
+        if (this.stream) {
             this.stream.stop();
         }
 
@@ -324,9 +256,9 @@ export default class DeviceList extends EventEmitter {
         this.stream.enumerate();
         if (!this.stream.current) return;
         // update current values
-        this.stream.current.forEach((descriptor: DeviceDescriptor) => {
-            const path: string = descriptor.path.toString();
-            const device: Device = this.devices[path];
+        this.stream.current.forEach((descriptor) => {
+            const path = descriptor.path.toString();
+            const device = this.devices[path];
             if (device) {
                 device.updateDescriptor(descriptor);
             }
@@ -339,10 +271,8 @@ export default class DeviceList extends EventEmitter {
  * returns instance of DeviceList
  * @returns {Promise<DeviceList>}
  */
-export const getDeviceList = async (): Promise<DeviceList> => {
-    const list = new DeviceList({
-        rememberDevicePassphrase: true,
-    });
+export const getDeviceList = async () => {
+    const list = new DeviceList();
     await list.init();
     return list;
 };
