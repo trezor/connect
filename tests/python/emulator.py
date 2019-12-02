09694a043c1f6d14e1518b91880d00f3b7251cc8
martin <mvarmuza@gmail.com>
2019-12-02 15:13:59 +0100

    lets start with tests in ci

--- tests/python/emulator.py
+++ tests/python/emulator.py
@@ -26,7 +26,10 @@ def start():
         # - run two T2/T1 emulators
         proc = Popen(
             # ["./emulator", "-O0", "-X", "heapsize=20M", "-m", "main"],
-            "./tests/python/emulator-mac -O0 -X heapsize=20M -m main",
+            # "./tests/python/emulator-mac -O0 -X heapsize=20M -m main",
+
+            "./tests/python/trezor-emu-core-v2.1.4 -O0 -X heapsize=20M -m main",
+
             shell=True,
             preexec_fn=os.setsid
         )
