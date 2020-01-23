31d5cff5cc74cd422aff2666b24e797a99441637
Szymon Lesisz <sz.lesisz@gmail.com>
2020-01-23 12:15:06 +0100

    add versionCompare util to replace 'semver-compare'

--- src/js/utils/arrayUtils.js
+++ src/js/utils/arrayUtils.js
@@ -1,9 +1,17 @@
 /* @flow */
 
-// export default function () {
-//     if (!Array.isArray) {
-//         Array.isArray = function(arg) {
-//             return Object.prototype.toString.call(arg) === '[object Array]';
-//         };
-//     }
-// }
+export const versionCompare = (a: string | number[], b: string | number[]) => {
+    const pa = typeof a === 'string' ? a.split('.') : a;
+    const pb = typeof b === 'string' ? b.split('.') : b;
+    if (!Array.isArray(pa) || !Array.isArray(pb)) return 0;
+    let i;
+    for (i = 0; i < 3; i++) {
+        const na = Number(pa[i]);
+        const nb = Number(pb[i]);
+        if (na > nb) return 1;
+        if (nb > na) return -1;
+        if (!isNaN(na) && isNaN(nb)) return 1;
+        if (isNaN(na) && !isNaN(nb)) return -1;
+    }
+    return 0;
+};
