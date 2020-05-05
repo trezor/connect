e6b87cd707f4a952b0672ba021bf38b1cbf76138
Szymon Lesisz <sz.lesisz@gmail.com>
2020-05-05 11:12:47 +0200

    Electron in renderer with popup example

--- examples/electron/renderer-with-popup/src/electron.js
+++ examples/electron/renderer-with-popup/src/electron.js
@@ -9,34 +9,12 @@ function init() {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 775,
-        // webPreferences: {
-        //     nodeIntegration: true, // enable nodejs integration, "trezor-connect" will use nodejs implementation
-        // },
     });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true,
     }));
-
-    // TODO: doesn't work with electron@7
-    // mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
-    //     console.log('NEW WINDOW!', frameName);
-    //     if (frameName === 'modal') {
-    //         // open window as modal
-    //         event.preventDefault()
-    //         options.webPreferences.affinity = 'main-window';
-    //         Object.assign(options, {
-    //             modal: true,
-    //             parent: mainWindow,
-    //             width: 780,
-    //             height: 620,
-    //             center: true,
-    //             closable: true, // doesn't work?, im not sure how to close this modal yet, should i render "close" button inside popup.html?
-    //         });
-    //         event.newGuest = new BrowserWindow(options);
-    //     }
-    // });
 
     // emitted when the window is closed.
     mainWindow.on('closed', () => {
