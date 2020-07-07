30a6f806b65eca1697875ff8943f4a2b2767ec11
Pavol Rusnak <pavol@rusnak.io>
2020-07-07 21:47:20 +0200

    nix: fix whitespace

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "trezor-connect-dev";
   buildInputs = [
     nodejs-12_x
-    (yarn.override { nodejs = nodejs-12_x; }) 
+    (yarn.override { nodejs = nodejs-12_x; })
   ];
   shellHook = ''
     export PATH="$PATH:$(pwd)/node_modules/.bin"
