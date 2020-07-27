a45ee129da40bedeaec66fc760da070e5c29a40c
1000101 <b1000101@pm.me>
2020-07-27 18:00:57 +0200

    shell: patch flow binary (#632)

--- shell.nix
+++ shell.nix
@@ -3,10 +3,12 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "trezor-connect-dev";
   buildInputs = [
+    autoPatchelfHook
     nodejs-12_x
     (yarn.override { nodejs = nodejs-12_x; })
   ];
   shellHook = ''
     export PATH="$PATH:$(pwd)/node_modules/.bin"
+    autoPatchelf $(pwd)/node_modules/flow-bin/
   '';
 }
