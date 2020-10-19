f4c35fe6e7fdd07c7838fa236c1b7b7117338966
Tomas Susanka <tsusanka@gmail.com>
2020-10-19 18:50:14 +0200

    nix: pin shell.nix to latest nixos-20.09 stable

--- shell.nix
+++ shell.nix
@@ -1,4 +1,10 @@
-with import <nixpkgs> {};
+# the last successful build of nixos-20.09 (stable) as of 2020-10-11
+with import
+  (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/0b8799ecaaf0dc6b4c11583a3c96ca5b40fcfdfb.tar.gz";
+    sha256 = "11m4aig6cv0zi3gbq2xn9by29cfvnsxgzf9qsvz67qr0yq29ryyz";
+  })
+{ };
 
 stdenv.mkDerivation {
   name = "trezor-connect-dev";
