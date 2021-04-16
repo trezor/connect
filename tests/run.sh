66bb70d4f5daed591b16ec23fba18770cd84a987
Szymon Lesisz <sz.lesisz@gmail.com>
2021-04-16 11:14:23 +0200

    ci: improve tests/run.sh script
    - add `d` param to disable docker usage
    - add `s` param to define test script (default: yarn test:integration)
    - replace `c` param to disable test cache
    - remove `g` param, GUI support is irrelevant
    - fixed "waitForEnv" endless loop, limited to 60 tries
    - user-env health check is done using curl, revert of 657db31ab478ec56b916eaba70683d62d20938c7
    - update docs

--- tests/run.sh
+++ tests/run.sh
@@ -1,93 +1,106 @@
 #!/usr/bin/env bash
 
-# This script allows you to run Connect tests locally. It spins up trezor-user-env
-# which the tests need to launch emulator and other things. Gitlab does not run
-# this script, because the tests run inside the trezor-user-env docker container
-# directly.
+# This script runs trezor-connect tests.
+# It spins up trezor-user-env and sets required evironment variables.
 
-set -e
+set -euo pipefail
 
-function cleanup() {
-  echo "Cleaning up"
-  echo "Stopping container with an ID $id"
-  # uncomment to show logs from container, useful for debugging
-  # docker logs $id
-  docker stop "$id" && echo "Stopped"
+USER_ENV_IMAGE="registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env"
+
+cleanup() {
+  if [ -n "${dockerID-}" ]; then
+    echo "Stopping container with an ID $dockerID"
+    docker stop "$dockerID" && echo "trezor-user-env stopped"
+  fi
 }
 
 trap cleanup EXIT
 
-run() {
-  # fetch latest image, can be commented out if you do not need latest master
+# Running standalone instance of trezor-user-env
+# docker run -it -e SDL_VIDEODRIVER="dummy" -p "9001:9001" -p "21326:21326" -p "21325:21326" registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env
+
+# Tweaking trezor-user-env locally
+# docker run -it -e SDL_VIDEODRIVER="dummy" -p "9001:9001" -p "21326:21326" -p "21325:21326" registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env nix-shell
+# do your changes using `vi` and run:
+# [nix-shell:/trezor-user-env]# ./run.sh
+
+# Ports
+# 9001  - websocket server, communication test case > user-env (setup etc...)
+# 21326 - trezord proxy. beacuse of trezord CORS check
+# 21325 - original trezord port redirected to trezor-user-env proxy
+
+runDocker() {
   echo "Pulling latest trezor-user-env"
-  docker pull registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env
+  docker pull "$USER_ENV_IMAGE"
+  dockerID=$(
+    docker run -d \
+      -e SDL_VIDEODRIVER="dummy" \
+      -p "9001:9001" \
+      -p "21326:21326" \
+      -p "21325:21326" \
+      "$USER_ENV_IMAGE"
+  )
+  docker logs -f "$dockerID" &
+  echo "Running docker container with ID $dockerID"
+}
 
-  if [ $GUI = false ]; then
-    id=$(
-      docker run -d \
-        -e SDL_VIDEODRIVER="dummy" \
-        -p "9001:9001" \
-        -p "21326:21326" \
-        -p "21325:21326" \
-        registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env \
-        "/trezor-user-env/run.sh"
-    )
-    echo "Running docker container with an ID:"
-    echo "$id"
+waitForEnv() {
+  echo "Waiting for trezor-user-env to load up..."
+  counter=0
+  max_attempts=60
 
-  else
-    xhost +
-    id=$(
-      docker run -d \
-        -e DISPLAY=:0 \
-        --network host \
-        -p "9001:9001" \
-        -p "21326:21326" \
-        -p "21325:21326" \
-        registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env \
-        "/trezor-user-env/run.sh"
-    )
-
-    echo "Running docker container with a GUI support with ID:"
-    echo "$id"
-  fi
-
-  echo "Waiting for the trezor-user-env to load up"
-  while ! netstat -tna | grep 'LISTEN\>' | grep -q ':9001\>'; do
-    echo "Waiting"
+  # there is no official support for websockets in curl
+  # trezor-user-env websocket server will recognize request with "close" header and close the connection
+  # otherwise curl will hang in streaming state
+  until (
+    curl -i -s -f \
+      -H "Connection: close" \
+      http://localhost:9001
+  ); do
+    if [ ${counter} -eq ${max_attempts} ]; then
+      echo "trezor-user-env is not running. exiting"
+      exit 1
+    fi
+    counter=$(($counter+1))
+    printf "."
     sleep 1
   done
-  echo "trezor-user-env loaded up"
 
-  echo "Running yarn"
-  yarn jest --config jest.config.integration.js --verbose --detectOpenHandles --forceExit --coverage $COVERAGE
+  echo "trezor-user-env loaded up"
 }
 
 show_usage() {
   echo "Usage: run [OPTIONS] [ARGS]"
   echo ""
   echo "Options:"
-  echo "  -g       Run tests with emulator graphical output"
-  echo "  -f       Use specific firmware version, for example: 2.1.4., 2.3.0"
-  echo "  -i       Included methods only, for example: applySettings,signTransaction"
-  echo "  -e       All methods except excluded, for example: applySettings,signTransaction"
-  echo "  -c       Collect coverage"
+  echo "  -c       Disable backend cache. default: enabled"
+  echo "  -d       Disable docker. Useful when running own instance of trezor-user-env. default: enabled"
+  echo "  -e       All methods except excluded, example: applySettings,signTransaction"
+  echo "  -f       Use specific firmware version, example: 2.1.4, 1.8.0 default: 2-master"
+  echo "  -i       Included methods only, example: applySettings,signTransaction"
+  echo "  -s       actual test script. default: 'yarn test:integration'"
 }
 
-FIRMWARE='2-master'
-INCLUDED_METHODS=''
-EXCLUDED_METHODS=''
-GUI=false
-COVERAGE=false
+# default options
+FIRMWARE="2-master"
+INCLUDED_METHODS=""
+EXCLUDED_METHODS=""
+DOCKER=true
+TEST_SCRIPT="yarn test:integration"
+USE_TX_CACHE=true
 
+# user options
 OPTIND=1
-while getopts ":i:e:f:hgc" opt; do
+while getopts ":i:e:f:s:hdc" opt; do
   case $opt in
-  c)
-    COVERAGE=true
+  d)
+    DOCKER=false
     ;;
-  g)
-    GUI=true
+  c)
+    USE_TX_CACHE=false
+    ;;
+  s)
+    TEST_SCRIPT=$OPTARG
     ;;
   f)
     FIRMWARE=$OPTARG
@@ -110,8 +123,27 @@ while getopts ":i:e:f:hgc" opt; do
 done
 shift $((OPTIND - 1))
 
+# export essential process.env variables
 export TESTS_FIRMWARE=$FIRMWARE
 export TESTS_INCLUDED_METHODS=$INCLUDED_METHODS
 export TESTS_EXCLUDED_METHODS=$EXCLUDED_METHODS
+export TESTS_USE_TX_CACHE=$USE_TX_CACHE
+
+run() {
+  if [ $DOCKER = true ]; then
+    runDocker
+  fi
+
+  waitForEnv
+
+  echo "Running ${TEST_SCRIPT}"
+  echo "  Firmware: ${FIRMWARE}"
+  echo "  Included methods: ${INCLUDED_METHODS}"
+  echo "  Excluded methods: ${EXCLUDED_METHODS}"
+  echo "  TxCache: ${USE_TX_CACHE}"
+
+  # run actual test script
+  ${TEST_SCRIPT}
+}
 
 run
