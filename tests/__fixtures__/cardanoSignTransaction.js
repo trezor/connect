08cfe712b77639487875b04215494de5a9ab019e
Szymon Lesisz <sz.lesisz@gmail.com>
2021-04-26 16:24:29 +0200

    test: fix cardanoSignTransaction fixtures
    related to changes in FW 2.3.7, PR 794

--- tests/__fixtures__/cardanoSignTransaction.js
+++ tests/__fixtures__/cardanoSignTransaction.js
@@ -493,6 +493,9 @@ export default {
 
         {
             description: 'signMetadata',
+            setup: {
+                firmware: [['2.0.0', '2.3.6']],
+            },
             params: {
                 inputs: [SAMPLE_INPUTS.shelley_input],
                 outputs: [SAMPLE_OUTPUTS.simple_shelley_output],
