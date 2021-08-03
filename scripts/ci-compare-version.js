42012b8781305806984f493da08d2ef7bd84f24f
Szymon Lesisz <sz.lesisz@gmail.com>
2021-08-03 12:13:15 +0200

    fix(ci): update version-check script
    - drop "compare-versions" dependency in favour of "semver"
    - add check for "prerelease" versions with -beta suffix

--- scripts/ci-compare-version.js
+++
@@ -1,9 +0,0 @@
-const compareVersions = require('compare-versions');
-
-if (compareVersions.compare(process.argv[2], process.argv[3], '>')) {
-    process.exit(0);
-}
-console.log(
-    'trezor-connect package.json version is the same or lower than the npm registry version.',
-);
-process.exit(1);
