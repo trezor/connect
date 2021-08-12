fbd990a16d399d4a4919994ae820a3bcee76763b
Frederik Bolding <frederik.bolding@gmail.com>
2021-08-12 18:01:17 +0200

    Add TS types

--- src/ts/types/networks/ethereum.d.ts
+++ src/ts/types/networks/ethereum.d.ts
@@ -35,9 +35,26 @@ export interface EthereumTransaction {
     txType?: number;
 }
 
+export type EthereumTransactionEIP1559 = {
+    to: string,
+    value: string,
+    gasLimit: string,
+    nonce: string,
+    data?: string,
+    chainId: number,
+    maxFeePerGas: string,
+    maxPriorityFeePerGas: string,
+    accessList?: EthereumAccessList[];
+};
+
+export type EthereumAccessList = {
+    address: string;
+    storageKeys: string[];
+};
+
 export interface EthereumSignTransaction {
     path: string | number[];
-    transaction: EthereumTransaction;
+    transaction: EthereumTransaction | EthereumTransactionEIP1559;
 }
 
 export interface EthereumSignedTx {
