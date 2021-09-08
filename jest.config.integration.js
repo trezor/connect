a70c6353d8e099959f3978045b9a3a782b23c4a2
Szymon Lesisz <sz.lesisz@gmail.com>
2021-09-08 12:28:12 +0200

    fix(cardano): update cbor dependency

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -6,6 +6,7 @@ module.exports = {
     rootDir: './',
     moduleFileExtensions: ['js'],
     testMatch: ['**/tests/device/**/*.test.js'],
+    testEnvironment: 'node',
     modulePathIgnorePatterns: ['node_modules', '_old', 'src/types', 'src/ui', 'src/utils/ws.ts'],
     setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js', '<rootDir>/tests/common.setup.js'],
     transform: {
