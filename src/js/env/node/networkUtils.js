36f33707f6917e5ee4fcc746fe3d49c04bebc277
Matej Kriz <matej@matejkriz.cz>
2021-09-08 15:52:34 +0200

    fix: file resolution of releases from connect-common

--- src/js/env/node/networkUtils.js
+++ src/js/env/node/networkUtils.js
@@ -1,48 +1,35 @@
 /* @flow */
+/* eslint-disable global-require */
 
-import fs from 'fs';
-import path from 'path';
 import nodeFetch from 'node-fetch';
 
 if (global && typeof global.fetch !== 'function') {
     global.fetch = nodeFetch;
 }
 
-const connectCommonFilesPath = path.resolve(
-    __dirname,
-    '../../../../node_modules/@trezor/connect-common/files/',
-);
-
-const getAbsolutePath = (fileUrl: string) => {
-    switch (fileUrl) {
-        case './data/bridge/releases.json':
-            return path.join(connectCommonFilesPath, 'bridge/releases.json');
-        case './data/firmware/1/releases.json':
-            return path.join(connectCommonFilesPath, 'firmware/1/releases.json');
-        case './data/firmware/2/releases.json':
-            return path.join(connectCommonFilesPath, 'firmware/2/releases.json');
-        default: {
-            return path.resolve(__dirname, '../../../', fileUrl);
-        }
-    }
-};
-
-export const httpRequest = (url: string, type: string): any => {
+export const httpRequest = (url: string, _type: string): any => {
     const fileUrl = url.split('?')[0];
-    const absolutePath = getAbsolutePath(fileUrl);
-    const content =
-        type !== 'binary'
-            ? fs.readFileSync(absolutePath, { encoding: 'utf8' })
-            : fs.readFileSync(absolutePath);
-    if (!content) return null;
 
-    if (type === 'binary') {
-        return Array.from(content);
+    switch (fileUrl) {
+        case './data/config.json':
+            return require('../../../data/config.json');
+        case './data/coins.json':
+            return require('../../../data/coins.json');
+        case './data/bridge/releases.json':
+            return require('@trezor/connect-common/files/bridge/releases.json');
+        case './data/firmware/1/releases.json':
+            return require('@trezor/connect-common/files/firmware/1/releases.json');
+        case './data/firmware/2/releases.json':
+            return require('@trezor/connect-common/files/firmware/2/releases.json');
+        case './data/messages/messages-v6.json':
+            return require('../../../data/messages/messages-v6.json');
+        case './data/messages/messages-v7.json':
+            return require('../../../data/messages/messages-v7.json');
+        case './data/messages/messages.json':
+            return require('../../../data/messages/messages.json');
+        default:
+            return null;
     }
-    if (type === 'json' && typeof content === 'string') {
-        return JSON.parse(content);
-    }
-    return content;
 };
 
 export const getOrigin = (url: string) => {
