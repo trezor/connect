98964f5396a444a802b721f2d005d51143b2e5b4
Szymon Lesisz <sz.lesisz@gmail.com>
2021-09-13 18:38:49 +0200

    tests: use trezor-common fixtures for ethereumSignTransaction

--- tests/__fixtures__/ethereumSignTransactionEip155.js
+++ tests/__fixtures__/ethereumSignTransactionEip155.js
@@ -1,4 +1,4 @@
-import commonFixtures from '../../submodules/trezor-common/tests/fixtures/ethereum/sign_tx_eip1559.json';
+import commonFixtures from '../../submodules/trezor-common/tests/fixtures/ethereum/sign_tx_eip155.json';
 
 export default {
     method: 'ethereumSignTransaction',
@@ -7,7 +7,7 @@ export default {
     },
     tests: commonFixtures.tests.flatMap(({ name, parameters, result }) => {
         const fixture = {
-            description: `Eip1559 ${name} ${parameters.comment ?? ''}`,
+            description: `Eip155 ${name} ${parameters.comment ?? ''}`,
             params: {
                 path: `m/${parameters.path}`,
                 transaction: {
@@ -16,23 +16,18 @@ export default {
                     value: parameters.value.toString(16),
                     nonce: parameters.nonce.toString(16),
                     gasLimit: parameters.gas_limit.toString(16),
-                    maxFeePerGas: parameters.max_gas_fee.toString(16),
-                    maxPriorityFeePerGas: parameters.max_priority_fee.toString(16),
+                    gasPrice: parameters.gas_price.toString(16),
                 },
             },
             result: {
-                r: `0x${result.sig_r_hex}`,
-                s: `0x${result.sig_s_hex}`,
+                r: `0x${result.sig_r}`,
+                s: `0x${result.sig_s}`,
                 v: `0x${result.sig_v.toString(16)}`,
             },
         };
 
         if (parameters.data) {
             fixture.params.transaction.data = parameters.data;
-        }
-
-        if (parameters.value >= Number.MAX_SAFE_INTEGER) {
-            fixture.params.transaction.value = '0xab54a98ceb1f0ad2'; // 12345678901234567890
         }
 
         return fixture;
