087c6c2a3497c057abaac1617fd803f8f912b9d3
Szymon Lesisz <sz.lesisz@gmail.com>
2021-09-13 18:38:49 +0200

    tests: use trezor-common fixtures for ethereumSignTransaction

--- tests/__fixtures__/ethereumGetAddress.js
+++ tests/__fixtures__/ethereumGetAddress.js
@@ -1,50 +1,13 @@
+import commonFixtures from '../../submodules/trezor-common/tests/fixtures/ethereum/getaddress.json';
+
 export default {
     method: 'ethereumGetAddress',
     setup: {
-        mnemonic: 'mnemonic_12',
+        mnemonic: commonFixtures.setup.mnemonic,
     },
-    tests: [
-        {
-            description: "m/44'/60'/1",
-            params: {
-                path: "m/44'/60'/1",
-            },
-            result: {
-                address: '0xeD46C856D0c79661cF7d40FFE0C0C5077c00E898',
-            },
-        },
-        {
-            description: "m/44'/60'/0/1'",
-            params: {
-                path: "m/44'/60'/0/1'",
-            },
-            result: {
-                address: '0x6682Fa7F3eC58581b1e576268b5463B4b5c93839',
-            },
-        },
-        {
-            description: "m/44'/60'/0'/9'/0",
-            params: {
-                path: "m/44'/60'/0'/9'/0",
-            },
-            result: {
-                address: '0x2cffCE5B7DA9584caD519EFc4715425b630CEF3a',
-            },
-        },
-        // possible since 2.3.6
-        // {
-        //     description: "m/44'/160'/0'/0/0",
-        //     params: {
-        //         path: "m/44'/160'/0'/0/0",
-        //     },
-        //     result: false,
-        // },
-        // {
-        //     description: "m/44'/199'/0'/0/9999",
-        //     params: {
-        //         path: "m/44'/199'/0'/0/9999",
-        //     },
-        //     result: false,
-        // },
-    ],
+    tests: commonFixtures.tests.flatMap(({ parameters: params, result }) => ({
+        description: params.path,
+        params,
+        result,
+    })),
 };
--- tests/__fixtures__/ethereumSignMessage.js
+++ tests/__fixtures__/ethereumSignMessage.js
@@ -1,32 +1,19 @@
+import commonFixtures from '../../submodules/trezor-common/tests/fixtures/ethereum/signmessage.json';
+
 export default {
     method: 'ethereumSignMessage',
     setup: {
-        mnemonic: 'mnemonic_12',
+        mnemonic: commonFixtures.setup.mnemonic,
     },
-    tests: [
-        {
-            description: "m/44'/60'/0'",
-            params: {
-                path: "m/44'/60'/0'",
-                message: 'This is an example of a signed message.',
-            },
-            result: {
-                address: '0xAE2B111b634f8FB3942B13b98c824B0F1060cacB',
-                signature:
-                    '9dc221f51fe1515d598324e51c1897637c3f0098b02758fa68a794803495df061ddbc1460b40497a80f3b1bf63e5966850c3bbd8ff91e0e9f4d7a121be32b1c21c',
-            },
+    tests: commonFixtures.tests.flatMap(({ parameters, result }) => ({
+        description: `${parameters.path} ${parameters.msg.substring(0, 30)}...`,
+        params: {
+            path: `m/${parameters.path}`,
+            message: parameters.msg,
         },
-        {
-            description: "m/44'/60'/0'",
-            params: {
-                path: "m/44'/60'/0'",
-                message: 'VeryLongMessage!'.repeat(64),
-            },
-            result: {
-                address: '0xAE2B111b634f8FB3942B13b98c824B0F1060cacB',
-                signature:
-                    '076d6dcf1fdcac60748919a781dc39d0ee7beece5fd5604be2f061e32ec6d91817562df343171ab38214fa3e2de46240ab330da34011856e2461378940243ec91c',
-            },
+        result: {
+            address: result.address,
+            signature: result.sig,
         },
-    ],
+    })),
 };
