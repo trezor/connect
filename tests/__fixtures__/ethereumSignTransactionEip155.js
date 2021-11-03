fa991781a40a3e7dfa1fb9518bde5439ac6ae0a3
mroz <mvarmuza@gmail.com>
2021-11-03 14:09:23 +0100

    test: exclude test with number out of safe integer range

--- tests/__fixtures__/ethereumSignTransactionEip155.js
+++ tests/__fixtures__/ethereumSignTransactionEip155.js
@@ -5,31 +5,34 @@ export default {
     setup: {
         mnemonic: commonFixtures.setup.mnemonic,
     },
-    tests: commonFixtures.tests.flatMap(({ name, parameters, result }) => {
-        const fixture = {
-            description: `Eip155 ${name} ${parameters.comment ?? ''}`,
-            params: {
-                path: parameters.path,
-                transaction: {
-                    to: parameters.to_address,
-                    chainId: parameters.chain_id,
-                    value: parameters.value,
-                    nonce: parameters.nonce,
-                    gasLimit: parameters.gas_limit,
-                    gasPrice: parameters.gas_price,
+    tests: commonFixtures.tests
+        // exclude test using integer value higher than Number.maxSafeInteger
+        .filter(f => !['max_uint64'].includes(f.name))
+        .flatMap(({ name, parameters, result }) => {
+            const fixture = {
+                description: `Eip155 ${name} ${parameters.comment ?? ''}`,
+                params: {
+                    path: parameters.path,
+                    transaction: {
+                        to: parameters.to_address,
+                        chainId: parameters.chain_id,
+                        value: parameters.value,
+                        nonce: parameters.nonce,
+                        gasLimit: parameters.gas_limit,
+                        gasPrice: parameters.gas_price,
+                    },
                 },
-            },
-            result: {
-                r: `0x${result.sig_r}`,
-                s: `0x${result.sig_s}`,
-                v: `0x${result.sig_v.toString(16)}`,
-            },
-        };
+                result: {
+                    r: `0x${result.sig_r}`,
+                    s: `0x${result.sig_s}`,
+                    v: `0x${result.sig_v.toString(16)}`,
+                },
+            };
 
-        if (parameters.data) {
-            fixture.params.transaction.data = parameters.data;
-        }
+            if (parameters.data) {
+                fixture.params.transaction.data = parameters.data;
+            }
 
-        return fixture;
-    }),
+            return fixture;
+        }),
 };
