066702b6b77e1b8565aa114387695fa6880957f9
Jun Luo <4catcode@gmail.com>
2021-11-24 11:02:38 +0800

    feat(stellar): ensure the parameter is supported by the hardware.

--- src/js/core/methods/StellarSignTransaction.js
+++ src/js/core/methods/StellarSignTransaction.js
@@ -6,12 +6,29 @@ import { getMiscNetwork } from '../../data/CoinInfo';
 import { validatePath } from '../../utils/pathUtils';
 import * as helper from './helpers/stellarSignTx';
 
-import type { CoreMessage } from '../../types';
+import type { CoreMessage, StellarTransaction } from '../../types';
+import { ERRORS } from '../../constants';
+
+const StellarSignTransactionFeatures = Object.freeze({
+    StellarCreateAccountOp: ['1.10.3', '2.4.2'],
+    StellarPaymentOp: ['1.10.3', '2.4.2'],
+    StellarPathPaymentStrictReceiveOp: ['1.10.4', '2.4.3'],
+    StellarPathPaymentStrictSendOp: ['1.10.4', '2.4.3'],
+    StellarCreatePassiveSellOfferOp: ['1.10.4', '2.4.3'],
+    StellarManageSellOfferOp: ['1.10.4', '2.4.3'],
+    StellarManageBuyOfferOp: ['1.10.4', '2.4.3'],
+    StellarSetOptionsOp: ['1.10.3', '2.4.2'],
+    StellarChangeTrustOp: ['1.10.3', '2.4.2'],
+    StellarAllowTrustOp: ['1.10.3', '2.4.2'],
+    StellarAccountMergeOp: ['1.10.3', '2.4.2'],
+    StellarManageDataOp: ['1.10.3', '2.4.2'],
+    StellarBumpSequenceOp: ['1.10.3', '2.4.2'],
+});
 
 type Params = {
     path: number[],
     networkPassphrase: string,
-    transaction: any,
+    transaction: StellarTransaction,
 };
 
 export default class StellarSignTransaction extends AbstractMethod {
@@ -45,7 +62,70 @@ export default class StellarSignTransaction extends AbstractMethod {
         };
     }
 
+    _isFeatureSupported(feature: $Keys<typeof StellarSignTransactionFeatures>) {
+        return this.device.atLeast(StellarSignTransactionFeatures[feature]);
+    }
+
+    _ensureFeatureIsSupported(feature: $Keys<typeof StellarSignTransactionFeatures>) {
+        if (!this._isFeatureSupported(feature)) {
+            throw ERRORS.TypedError(
+                'Method_InvalidParameter',
+                `Feature ${feature} not supported by device firmware`,
+            );
+        }
+    }
+
+    _ensureFirmwareSupportsParams() {
+        const { params } = this;
+        params.transaction.operations.forEach(operation => {
+            switch (operation.type) {
+                case 'createAccount':
+                    this._ensureFeatureIsSupported('StellarCreateAccountOp');
+                    break;
+                case 'payment':
+                    this._ensureFeatureIsSupported('StellarPaymentOp');
+                    break;
+                case 'pathPaymentStrictReceive':
+                    this._ensureFeatureIsSupported('StellarPathPaymentStrictReceiveOp');
+                    break;
+                case 'pathPaymentStrictSend':
+                    this._ensureFeatureIsSupported('StellarPathPaymentStrictSendOp');
+                    break;
+                case 'createPassiveSellOffer':
+                    this._ensureFeatureIsSupported('StellarCreatePassiveSellOfferOp');
+                    break;
+                case 'manageSellOffer':
+                    this._ensureFeatureIsSupported('StellarManageSellOfferOp');
+                    break;
+                case 'manageBuyOffer':
+                    this._ensureFeatureIsSupported('StellarManageBuyOfferOp');
+                    break;
+                case 'setOptions':
+                    this._ensureFeatureIsSupported('StellarSetOptionsOp');
+                    break;
+                case 'changeTrust':
+                    this._ensureFeatureIsSupported('StellarChangeTrustOp');
+                    break;
+                case 'accountMerge':
+                    this._ensureFeatureIsSupported('StellarAccountMergeOp');
+                    break;
+                case 'manageData':
+                    this._ensureFeatureIsSupported('StellarManageDataOp');
+                    break;
+                case 'bumpSequence':
+                    this._ensureFeatureIsSupported('StellarBumpSequenceOp');
+                    break;
+                case 'inflation':
+                    // InflationOp has been deprecated and is not supported by Trezor
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
     async run() {
+        this._ensureFirmwareSupportsParams();
         const response = await helper.stellarSignTx(
             this.device.getCommands().typedCall.bind(this.device.getCommands()),
             this.params.path,
