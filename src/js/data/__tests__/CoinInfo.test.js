77011bb29c466de3c6e8e1e4be87c159aba15e55
mroz <mvarmuza@gmail.com>
2021-12-14 13:55:21 +0100

    chore: remove unused getCoinInfoByCapability

--- src/js/data/__tests__/CoinInfo.test.js
+++ src/js/data/__tests__/CoinInfo.test.js
@@ -1,10 +1,5 @@
 import coinsJSON from '../../../data/coins.json';
-import {
-    parseCoinsJson,
-    getCoinInfo,
-    getUniqueNetworks,
-    getCoinInfoByCapability,
-} from '../CoinInfo';
+import { parseCoinsJson, getCoinInfo, getUniqueNetworks } from '../CoinInfo';
 
 describe('data/CoinInfo', () => {
     beforeAll(() => {
@@ -22,51 +17,5 @@ describe('data/CoinInfo', () => {
         ];
         const result = [getCoinInfo('btc'), getCoinInfo('ltc')];
         expect(getUniqueNetworks(inputs)).toEqual(result);
-    });
-
-    it('getCoinInfoByCapability', () => {
-        expect(getCoinInfoByCapability(['Capability_Bitcoin'])).toMatchObject([
-            { name: 'Bitcoin' },
-            { name: 'Testnet' },
-        ]);
-
-        expect(
-            getCoinInfoByCapability(['Capability_Bitcoin', 'Capability_Bitcoin_like']).length,
-        ).toEqual(coinsJSON.bitcoin.length);
-
-        expect(getCoinInfoByCapability(['Capability_Ethereum']).length).toEqual(
-            coinsJSON.eth.length,
-        );
-
-        const other = ['EOS', 'NEM', 'Stellar', 'Tezos'];
-
-        other.forEach(c => {
-            expect(getCoinInfoByCapability([`Capability_${c}`])).toMatchObject([{ name: c }]);
-        });
-
-        expect(getCoinInfoByCapability(['Capability_Binance'])).toMatchObject([
-            { name: 'Binance Chain', shortcut: 'BNB' },
-        ]);
-
-        expect(getCoinInfoByCapability(['Capability_Ripple'])).toMatchObject([
-            { name: 'Ripple' },
-            { name: 'Ripple Testnet' },
-        ]);
-
-        expect(getCoinInfoByCapability(['Capability_U2F'])).toEqual([]);
-        expect(getCoinInfoByCapability(['Capability_Monero'])).toEqual([]);
-
-        const all = [
-            'Capability_Bitcoin',
-            'Capability_Bitcoin_like',
-            'Capability_Binance',
-            'Capability_Ethereum',
-            'Capability_Ripple',
-            'Capability_Cardano',
-        ].concat(other.map(c => `Capability_${c}`));
-
-        expect(getCoinInfoByCapability(all).length).toEqual(
-            coinsJSON.bitcoin.length + coinsJSON.eth.length + coinsJSON.misc.length,
-        );
     });
 });
