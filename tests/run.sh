35c4c7d09e55f4ae7c9c3cee1ffba3d18ef13c34
Alois Klink <alois.klink@gmail.com>
2021-12-19 00:20:47 +0000

    test: Add unofficial support for `podman`
    Podman is an open-source daemonless way to run containers in Linux.
    
    Essentially, this means it doesn't need `root` access to run,
    so it's slightly more secure.
    
    Its CLI is so similar to `docker`, that you can just use
    `alias docker=podman` and it should work in 99.99% of cases.
    
    However, `alias docker=podman` doesn't work in bash scripts
    by default, so I've added a flag `-D PATH` to set the docker path,
    which also allows replacing it with `podman`.

--- tests/run.sh
+++ tests/run.sh
@@ -10,7 +10,7 @@ USER_ENV_IMAGE="registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-us
 cleanup() {
   if [ -n "${dockerID-}" ]; then
     echo "Stopping container with an ID $dockerID"
-    docker stop "$dockerID" && echo "trezor-user-env stopped"
+    "$DOCKER_PATH" stop "$dockerID" && echo "trezor-user-env stopped"
   fi
 }
 
@@ -31,16 +31,16 @@ trap cleanup EXIT
 
 runDocker() {
   echo "Pulling latest trezor-user-env"
-  docker pull "$USER_ENV_IMAGE"
+  "$DOCKER_PATH" pull "$USER_ENV_IMAGE"
   dockerID=$(
-    docker run -d \
+    "$DOCKER_PATH" run -d \
       -e SDL_VIDEODRIVER="dummy" \
       -p "9001:9001" \
       -p "21326:21326" \
       -p "21325:21326" \
       "$USER_ENV_IMAGE"
   )
-  docker logs -f "$dockerID" &
+  "$DOCKER_PATH" logs -f "$dockerID" &
   echo "Running docker container with ID $dockerID"
 }
 
@@ -70,6 +70,7 @@ show_usage() {
   echo "Options:"
   echo "  -c       Disable backend cache. default: enabled"
   echo "  -d       Disable docker. Useful when running own instance of trezor-user-env. default: enabled"
+  echo "  -D PATH  Set path to docker executable. Can be replaced with `podman`. default: docker"
   echo "  -e       All methods except excluded, example: applySettings,signTransaction"
   echo "  -f       Use specific firmware version, example: 2.1.4, 1.8.0 default: 2-master"
   echo "  -i       Included methods only, example: applySettings,signTransaction"
@@ -86,16 +87,20 @@ FIRMWARE=$RELEASED_FIRMWARE
 INCLUDED_METHODS=""
 EXCLUDED_METHODS=""
 DOCKER=true
+DOCKER_PATH="docker"
 TEST_SCRIPT="yarn test:integration"
 USE_TX_CACHE=true
 USE_WS_CACHE=true
 
 # user options
 OPTIND=1
-while getopts ":i:e:f:s:hdc" opt; do
+while getopts ":i:e:f:s:D:hdc" opt; do
   case $opt in
   d)
     DOCKER=false
+    ;;
+  D)
+    DOCKER_PATH="$OPTARG"
     ;;
   c)
     USE_TX_CACHE=false
