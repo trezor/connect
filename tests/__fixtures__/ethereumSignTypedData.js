0a65cffed5eb552599ed97b851a402a2b5d66bc9
mroz <mvarmuza@gmail.com>
2022-01-12 19:34:35 +0100

    tests: better support for tests against model One

--- tests/__fixtures__/ethereumSignTypedData.js
+++ tests/__fixtures__/ethereumSignTypedData.js
@@ -1,20 +1,47 @@
 import commonFixtures from '../../submodules/trezor-common/tests/fixtures/ethereum/sign_typed_data.json';
+// import transformTypedData from '../../src/js/plugins/ethereum/typedData';
+
+const fixtures = commonFixtures.tests.flatMap(({ name, parameters, result }) => {
+    const fixture = {
+        setup: {
+            firmware: [['2.4.2', '2-master']],
+        },
+        description: `${name} ${parameters.comment ?? ''}`,
+        name,
+        params: parameters,
+        result: {
+            address: result.address,
+            signature: result.sig,
+        },
+    };
+    return fixture;
+});
+
+// const t1Fixtures = fixtures.map(f => {
+//     const fixture = {
+//         ...f,
+//         description: `t1: ${f.description}`,
+//         setup: {
+//             ...f.setup,
+//             firmware: [['1.10.6', '1-master']],
+//         },
+//         params: {
+//             ...f.params,
+//             ...transformTypedData(f.params.data, true),
+//         },
+//     };
+
+//     if (!f.params.metamask_v4_compat) {
+//         fixture.result = false;
+//     }
+//     return fixture;
+// });
 
 export default {
     method: 'ethereumSignTypedData',
     setup: {
         mnemonic: commonFixtures.setup.mnemonic,
     },
-    tests: commonFixtures.tests.flatMap(({ name, parameters, result }) => {
-        const fixture = {
-            description: `${name} ${parameters.comment ?? ''}`,
-            name,
-            params: parameters,
-            result: {
-                address: result.address,
-                signature: result.sig,
-            },
-        };
-        return fixture;
-    }),
+    // tests: [...fixtures, ...t1Fixtures],
+    tests: [...fixtures],
 };
