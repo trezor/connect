2ad21b03bdfd63e54c5ca2427bfbe12cb036c1f3
Alois Klink <alois.klink@gmail.com>
2022-02-04 14:46:45 +0000

    fix(plugins/ethereum): Support domain-only EIP712
    Adds support for domain-only EIP-712 hashes to
    plugins/ethereum/typedData.js

--- src/js/plugins/ethereum/typedData.js
+++ src/js/plugins/ethereum/typedData.js
@@ -20,6 +20,17 @@ function sanitizeData(data) {
     }
 }
 
+/**
+ * Calculates the domain_separator_hash and message_hash from an EIP-712 Typed Data object.
+ *
+ * The Trezor Model 1 does not currently support constructing the hash on the device,
+ * so this function pre-computes them.
+ *
+ * @template {sigUtil.TypedMessage} T
+ * @param {T} data - The EIP-712 Typed Data object.
+ * @param {boolean} metamask_v4_compat - Set to `true` for compatibility with Metamask's signTypedData_v4 function.
+ * @returns {{domain_separator_hash: string, message_hash?: string} & T} The hashes.
+ */
 const transformTypedData = (data, metamask_v4_compat) => {
     if (!metamask_v4_compat) {
         throw new Error('Trezor: Only version 4 of typed data signing is supported');
@@ -36,12 +47,16 @@ const transformTypedData = (data, metamask_v4_compat) => {
         version,
     ).toString('hex');
 
-    const messageHash = sigUtil.TypedDataUtils.hashStruct(
-        primaryType,
-        sanitizeData(message),
-        types,
-        version,
-    ).toString('hex');
+    let messageHash = null;
+
+    if (primaryType !== 'EIP712Domain') {
+        messageHash = sigUtil.TypedDataUtils.hashStruct(
+            primaryType,
+            sanitizeData(message),
+            types,
+            version,
+        ).toString('hex');
+    }
 
     return {
         domain_separator_hash: domainSeparatorHash,
