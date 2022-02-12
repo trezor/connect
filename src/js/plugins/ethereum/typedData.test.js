b0fef6a8d3f47fc7f97e00c422e2c5053842be6c
Alois Klink <alois.klink@gmail.com>
2022-02-12 23:55:01 +0000

    test(plugins/ethereum): Improve test names
    Each test now has a unique name, taken from the fixture name.

--- src/js/plugins/ethereum/typedData.test.js
+++ src/js/plugins/ethereum/typedData.test.js
@@ -8,26 +8,28 @@ function messageToHex(string) {
 }
 
 describe('typedData', () => {
-    commonFixtures.tests
-        .filter(test => test.parameters.metamask_v4_compat)
-        .forEach(test => {
-            it('typedData to message_hash and domain_separator_hash', () => {
-                const transformed = typedData(
-                    test.parameters.data,
-                    test.parameters.metamask_v4_compat,
-                );
-                const { domain_separator_hash, message_hash } = transformed;
-
-                expect(messageToHex(domain_separator_hash)).toEqual(
-                    messageToHex(test.parameters.domain_separator_hash),
-                );
-                if (message_hash) {
-                    expect(messageToHex(message_hash)).toEqual(
-                        messageToHex(test.parameters.message_hash),
+    describe('should create message_hash and domain_separator_hash from typedData', () => {
+        commonFixtures.tests
+            .filter(test => test.parameters.metamask_v4_compat)
+            .forEach(test => {
+                it(test.name, () => {
+                    const transformed = typedData(
+                        test.parameters.data,
+                        test.parameters.metamask_v4_compat,
                     );
-                } else {
-                    expect(null).toEqual(test.parameters.message_hash);
-                }
+                    const { domain_separator_hash, message_hash } = transformed;
+
+                    expect(messageToHex(domain_separator_hash)).toEqual(
+                        messageToHex(test.parameters.domain_separator_hash),
+                    );
+                    if (message_hash) {
+                        expect(messageToHex(message_hash)).toEqual(
+                            messageToHex(test.parameters.message_hash),
+                        );
+                    } else {
+                        expect(null).toEqual(test.parameters.message_hash);
+                    }
+                });
             });
-        });
+    });
 });
